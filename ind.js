6433a3a79740b645c7b50dc6c44c43339f830bd6
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-27 08:52:10 +1000

    Update ind.js

--- ind.js
+++ ind.js
@@ -2,60 +2,37 @@ const fs = require('fs');
 const { RSI, EMA, ROC, BollingerBands, ADX, OBV, PSAR, WMA } = require('technicalindicators');
 
 const rawData = fs.readFileSync('price.json');
-const candles = JSON.parse(rawData);
-
-const closePrices = candles.map(candle => candle.close);
+const data = JSON.parse(rawData);
 
 const rsiPeriod = 14;
-const rsiValues = RSI.calculate({ period: rsiPeriod, values: closePrices });
-
 const emaPeriod = 20;
-const emaValues = EMA.calculate({ period: emaPeriod, values: closePrices });
-
 const rocPeriod = 12;
-const rocValues = ROC.calculate({ period: rocPeriod, values: closePrices });
-
 const bbPeriod = 20;
-const bbValues = BollingerBands.calculate({ period: bbPeriod, values: closePrices, stdDev: 2 });
-
+const bbStdDev = 2;
 const adxPeriod = 14;
-const adxValues = ADX.calculate({ period: adxPeriod, high: candles.map(candle => candle.high), low: candles.map(candle => candle.low), close: closePrices });
+const psarAccelerationFactor = 0.02;
+const psarMaxAccelerationFactor = 0.2;
+const wmaPeriod = 20;
 
-const obvValues = OBV.calculate({ close: closePrices, volume: candles.map(candle => candle.volume) });
+const rsiValues = RSI.calculate({ period: rsiPeriod, values: data.map(candle => candle.close) });
+const emaValues = EMA.calculate({ period: emaPeriod, values: data.map(candle => candle.close) });
+const rocValues = ROC.calculate({ period: rocPeriod, values: data.map(candle => candle.close) });
+const bbValues = BollingerBands.calculate({ period: bbPeriod, stdDev: bbStdDev, values: data.map(candle => candle.close) });
+const adxValues = ADX.calculate({ period: adxPeriod, high: data.map(candle => candle.high), low: data.map(candle => candle.low), close: data.map(candle => candle.close) });
+const obvValues = OBV.calculate({ close: data.map(candle => candle.close), volume: data.map(candle => candle.volume) });
+const psarValues = PSAR.calculate({ accelerationFactor: psarAccelerationFactor, maxAccelerationFactor: psarMaxAccelerationFactor, high: data.map(candle => candle.high), low: data.map(candle => candle.low) });
+const wmaValues = WMA.calculate({ period: wmaPeriod, values: data.map(candle => candle.close) });
 
-const psarValues = PSAR.calculate({ high: candles.map(candle => candle.high), low: candles.map(candle => candle.low) });
-
-const wmaPeriod = 10;
-const wmaValues = WMA.calculate({ period: wmaPeriod, values: closePrices });
-/*
-console.log('RSI:', rsiValues);
-console.log('EMA:', emaValues);
-console.log('ROC:', rocValues);
-console.log('Bollinger Bands:', bbValues);
-console.log('ADX:', adxValues);
-console.log('OBV:', obvValues);
-console.log('PSAR:', psarValues);
-console.log('WMA:', wmaValues);*/
 const result = {
-
-  RSI: rsiValues,
-
-  EMA: emaValues,
-
-  ROC: rocValues,
-
-  'Bollinger Bands': bbValues,
-
-  ADX: adxValues,
-
-  OBV: obvValues,
-
-  PSAR: psarValues,
-
-  WMA: wmaValues,
-
+  rsi: rsiValues,
+  ema: emaValues,
+  roc: rocValues,
+  bb: bbValues,
+  adx: adxValues,
+  obv: obvValues,
+  psar: psarValues,
+  wma: wmaValues,
 };
 
 fs.writeFileSync('indres.json', JSON.stringify(result));
-
 console.log('Indicators saved to indres.json');
