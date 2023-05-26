7f5475f93766cc73d6a368e7828372ef576f93ab
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 22:44:59 +1000

    Create patt.js

---
+++ patt.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { bearishEngulfingPattern, bullishEngulfingPattern, doji, bullishHammer, bearishHammer } = require('technicalindicators');
+
+const rawData = fs.readFileSync('price.json');
+const candles = JSON.parse(rawData);
+
+const lastFiveCandles = candles.slice(-5);
+
+const bearishEngulfing = bearishEngulfingPattern(lastFiveCandles);
+const bullishEngulfing = bullishEngulfingPattern(lastFiveCandles);
+const dojiPattern = doji(lastFiveCandles);
+const bullishHammerPattern = bullishHammer(lastFiveCandles);
+const bearishHammerPattern = bearishHammer(lastFiveCandles);
+
+const result = {
+  'Bearish Engulfing Pattern': bearishEngulfing,
+  'Bullish Engulfing Pattern': bullishEngulfing,
+  'Doji Pattern': dojiPattern,
+  'Bullish Hammer Pattern': bullishHammerPattern,
+  'Bearish Hammer Pattern': bearishHammerPattern,
+};
+
+fs.writeFileSync('pattres.json', JSON.stringify(result));
+console.log('Patterns saved to pattres.json');
