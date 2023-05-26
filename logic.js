abd9e293ee0eabac633d2404d16c1e867337c162
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-27 08:53:10 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,67 +1,90 @@
 const fs = require('fs');
-const technicalIndicators = require('technicalindicators');
+const { RSI, EMA, ROC, BollingerBands, ADX, OBV, PSAR, WMA } = require('technicalindicators');
 
 const rawData = fs.readFileSync('price.json');
 const data = JSON.parse(rawData);
 
-const rawIndicators = fs.readFileSync('indres.json');
-const indicators = JSON.parse(rawIndicators);
+const indData = fs.readFileSync('indres.json');
+const indicators = JSON.parse(indData);
 
-const close = data.map(candle => candle.close);
-const high = data.map(candle => candle.high);
-const low = data.map(candle => candle.low);
+const currentPrice = data[data.length - 1].close;
 
-const currentPrice = close[close.length - 1];
+// Определение текущего тренда
+const ema20 = indicators.ema[indicators.ema.length - 1];
+const ema50 = EMA.calculate({ period: 50, values: data.map(candle => candle.close) })[data.length - 1];
+const ema200 = EMA.calculate({ period: 200, values: data.map(candle => candle.close) })[data.length - 1];
 
-const trend = {
-  current: currentPrice > indicators.ema[indicators.ema.length - 1] ? 'up' : 'down',
-  global: close[0] > indicators.ema[indicators.ema.length - 1] ? 'up' : 'down',
-  '4h': close[close.length - 5] > indicators.ema[indicators.ema.length - 5] ? 'up' : 'down',
-  '12h': close[close.length - 13] > indicators.ema[indicators.ema.length - 13] ? 'up' : 'down',
-  '24h': close[close.length - 25] > indicators.ema[indicators.ema.length - 25] ? 'up' : 'down',
-};
+let currentTrend = '';
+if (currentPrice > ema20 && ema20 > ema50 && ema50 > ema200) {
+  currentTrend = 'Восходящий';
+} else if (currentPrice < ema20 && ema20 < ema50 && ema50 < ema200) {
+  currentTrend = 'Нисходящий';
+} else {
+  currentTrend = 'Боковой';
+}
 
-const supportResistance = {
-  current: {
-    support: technicalIndicators.PivotPoint.calculate({ high, low, close })[0].support1,
-    resistance: technicalIndicators.PivotPoint.calculate({ high, low, close })[0].resistance1,
-  },
-  '4h': {
-    support: technicalIndicators.PivotPoint.calculate({ high: high.slice(-20), low: low.slice(-20), close: close.slice(-20) })[0].support1,
-    resistance: technicalIndicators.PivotPoint.calculate({ high: high.slice(-20), low: low.slice(-20), close: close.slice(-20) })[0].resistance1,
-  },
-  '12h': {
-    support: technicalIndicators.PivotPoint.calculate({ high: high.slice(-60), low: low.slice(-60), close: close.slice(-60) })[0].support1,
-    resistance: technicalIndicators.PivotPoint.calculate({ high: high.slice(-60), low: low.slice(-60), close: close.slice(-60) })[0].resistance1,
-  },
-  '24h': {
-    support: technicalIndicators.PivotPoint.calculate({ high: high.slice(-120), low: low.slice(-120), close: close.slice(-120) })[0].support1,
-    resistance: technicalIndicators.PivotPoint.calculate({ high: high.slice(-120), low: low.slice(-120), close: close.slice(-120) })[0].resistance1,
-  },
-};
+// Определение глобального тренда
+const ema200Daily = EMA.calculate({ period: 200, values: data.slice(-24 * 60).map(candle => candle.close) })[0];
+const globalTrend = currentPrice > ema200Daily ? 'Восходящий' : 'Нисходящий';
 
-const overboughtOversold = {
-  rsi: indicators.rsi[indicators.rsi.length - 1] > 70 ? 'overbought' : indicators.rsi[indicators.rsi.length - 1] < 30 ? 'oversold' : 'neutral',
-  roc: indicators.roc[indicators.roc.length - 1] > 10 ? 'overbought' : indicators.roc[indicators.roc.length - 1] < -10 ? 'oversold' : 'neutral',
-};
+// Определение уровней поддержки и сопротивления
+const support1 = indicators.bb[indicators.bb.length - 1].lower;
+const support2 = indicators.bb[indicators.bb.length - 1].lower - (indicators.bb[indicators.bb.length - 1].upper - indicators.bb[indicators.bb.length - 1].lower) / 2;
+const resistance1 = indicators.bb[indicators.bb.length - 1].upper;
+const resistance2 = indicators.bb[indicators.bb.length - 1].upper + (indicators.bb[indicators.bb.length - 1].upper - indicators.bb[indicators.bb.length - 1].lower) / 2;
 
-const recommendations = {
-  buy: {
-    current: currentPrice < supportResistance.current.support ? { price: supportResistance.current.support, message: 'Buy at support level' } : { price: currentPrice, message: 'Buy at current price' },
-    '4h': currentPrice < supportResistance['4h'].support ? { price: supportResistance['4h'].support, message: 'Buy at 4h support level' } : { price: currentPrice, message: 'Buy at current price' },
-    '12h': currentPrice < supportResistance['12h'].support ? { price: supportResistance['12h'].support, message: 'Buy at 12h support level' } : { price: currentPrice, message: 'Buy at current price' },
-    '24h': currentPrice < supportResistance['24h'].support ? { price: supportResistance['24h'].support, message: 'Buy at 24h support level' } : { price: currentPrice, message: 'Buy at current price' },
-  },
-  sell: {
-    current: currentPrice > supportResistance.current.resistance ? { price: supportResistance.current.resistance, message: 'Sell at resistance level' } : { price: currentPrice, message: 'Sell at current price' },
-    '4h': currentPrice > supportResistance['4h'].resistance ? { price: supportResistance['4h'].resistance, message: 'Sell at 4h resistance level' } : { price: currentPrice, message: 'Sell at current price' },
-    '12h': currentPrice > supportResistance['12h'].resistance ? { price: supportResistance['12h'].resistance, message: 'Sell at 12h resistance level' } : { price: currentPrice, message: 'Sell at current price' },
-    '24h': currentPrice > supportResistance['24h'].resistance ? { price: supportResistance['24h'].resistance, message: 'Sell at 24h resistance level' } : { price: currentPrice, message: 'Sell at current price' },
-  },
-};
+const support4h = indicators.bb[indicators.bb.length - 7].lower;
+const support12h = indicators.bb[indicators.bb.length - 25].lower;
+const support24h = indicators.bb[indicators.bb.length - 49].lower;
+const resistance4h = indicators.bb[indicators.bb.length - 7].upper;
+const resistance12h = indicators.bb[indicators.bb.length - 25].upper;
+const resistance24h = indicators.bb[indicators.bb.length - 49].upper;
 
-console.log(`Current price: ${currentPrice}`);
-console.log(`Trend: current - ${trend.current}, global - ${trend.global}, 4h - ${trend['4h']}, 12h - ${trend['12h']}, 24h - ${trend['24h']}`);
-console.log(`Support and resistance levels: current - support ${supportResistance.current.support}, resistance ${supportResistance.current.resistance}, 4h - support ${supportResistance['4h'].support}, resistance ${supportResistance['4h'].resistance}, 12h - support ${supportResistance['12h'].support}, resistance ${supportResistance['12h'].resistance}, 24h - support ${supportResistance['24h'].support}, resistance ${supportResistance['24h'].resistance}`);
-console.log(`Overbought/oversold: RSI - ${overboughtOversold.rsi}, ROC - ${overboughtOversold.roc}`);
-console.log(`Recommendations: Buy - ${recommendations.buy.current.message} (${recommendations.buy.current.price}), Sell - ${recommendations.sell.current.message} (${recommendations.sell.current.price})`);
+// Определение перекупленности/перепроданности рынка
+const rsi14 = indicators.rsi[indicators.rsi.length - 1];
+let marketState = '';
+if (rsi14 > 70) {
+  marketState = 'Перекуплен';
+} else if (rsi14 < 30) {
+  marketState = 'Перепродан';
+} else {
+  marketState = 'Нормальное состояние';
+}
+
+// Определение рекомендаций с точками входа-выхода в сделку
+let recommendation = '';
+let entryPrice = 0;
+let exitPrice = 0;
+if (currentPrice < support1) {
+  recommendation = 'Покупать';
+  entryPrice = currentPrice;
+  exitPrice = resistance1;
+} else if (currentPrice > resistance1) {
+  recommendation = 'Продавать';
+  entryPrice = currentPrice;
+  exitPrice = support1;
+} else {
+  recommendation = 'Ждать';
+}
+
+// Определение рекомендаций о покупке и продаже
+let buySellRecommendation = '';
+if (currentPrice < ema20 && ema20 < ema50 && ema50 < ema200) {
+  buySellRecommendation = 'Покупать';
+} else if (currentPrice > ema20 && ema20 > ema50 && ema50 > ema200) {
+  buySellRecommendation = 'Продавать';
+} else {
+  buySellRecommendation = 'Ждать';
+}
+
+console.log(`Текущая цена: ${currentPrice}`);
+console.log(`Текущий тренд: ${currentTrend}`);
+console.log(`Глобальный тренд: ${globalTrend}`);
+console.log(`Уровни поддержки: ${support1.toFixed(2)}, ${support2.toFixed(2)}`);
+console.log(`Уровни сопротивления: ${resistance1.toFixed(2)}, ${resistance2.toFixed(2)}`);
+console.log(`Уровни поддержки и сопротивления за последние 4 часа: ${support4h.toFixed(2)}, ${resistance4h.toFixed(2)}`);
+console.log(`Уровни поддержки и сопротивления за последние 12 часов: ${support12h.toFixed(2)}, ${resistance12h.toFixed(2)}`);
+console.log(`Уровни поддержки и сопротивления за последние 24 часа: ${support24h.toFixed(2)}, ${resistance24h.toFixed(2)}`);
+console.log(`Состояние рынка: ${marketState}`);
+console.log(`Рекомендация с точками входа-выхода в сделку: ${recommendation} (вход: ${entryPrice.toFixed(2)}, выход: ${exitPrice.toFixed(2)})`);
+console.log(`Рекомендация о покупке и продаже: ${buySellRecommendation}`);
