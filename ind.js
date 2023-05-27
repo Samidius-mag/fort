4d812e3f516f095dadb059a9fb2e2327179cd5e5
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-27 18:27:03 +1000

    Update ind.js

--- ind.js
+++ ind.js
@@ -34,7 +34,7 @@ const psarValues = PSAR.calculate({ high: priceData.map(candle => parseFloat(can
 const psar = psarValues.map(value => parseFloat(value.toFixed(2)));
 
 // WMA
-const wmaPeriod = 10;
+const wmaPeriod = 50;
 const wmaValues = WMA.calculate({ period: wmaPeriod, values: close });
 const wma = wmaValues.map(value => parseFloat(value.toFixed(2)));
 
