b5113b71c37607eb4766272ef09bd7b6ba7b4663
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-29 09:54:46 +1000

    Update patt.js

--- patt.js
+++ patt.js
@@ -1,12 +1,15 @@
-const { bullishhammer } = require('technicalindicators');
-const data = require('./price.json');
+const fs = require('fs');
 
-const input = {
-  open: data.open.slice(-2),
-  high: data.high.slice(-2),
-  low: data.low.slice(-2),
-  close: data.close.slice(-2)
-};
+const data = JSON.parse(fs.readFileSync('price.json'));
 
-const isBullishHammer = bullishhammer(input);
-console.log('Is bullish hammer:', isBullishHammer);
+const candle = data[data.length - 2];
+
+const bodyLength = Math.abs(candle.open - candle.close);
+const upperShadowLength = candle.high - Math.max(candle.open, candle.close);
+const lowerShadowLength = Math.min(candle.open, candle.close) - candle.low;
+
+const isBullishHammer = bodyLength < upperShadowLength &&
+  bodyLength <= candle.high / 3 &&
+  lowerShadowLength <= bodyLength / 2;
+
+console.log(`Is Bullish Hammer: ${isBullishHammer}`);
