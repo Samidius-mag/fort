8167fb1677563eebedf58f971181b0095bcd4374
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-28 19:09:05 +1000

    Update tf.js

--- tf.js
+++ tf.js
@@ -12,15 +12,20 @@ const input = [
   indRes.OBV.slice(0, -24),
   indRes.WMA.slice(0, -24),
 ];
+const maxLength = Math.max(...input.map(arr => arr.length));
+const paddedInput = input.map(arr => {
+  const padding = Array(maxLength - arr.length).fill(0);
+  return [...arr, ...padding];
+});
 const output = prices.slice(24);
 
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 64, inputShape: [input.length], activation: 'relu' }));
+model.add(tf.layers.dense({ units: 64, inputShape: [paddedInput.length], activation: 'relu' }));
 model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
 model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
 model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
 
-const xs = tf.tensor2d(input.slice(-1)[0], [1, input[0].length]);
+const xs = tf.tensor2d(paddedInput.slice(-1)[0], [1, paddedInput[0].length]);
 const ys = tf.tensor2d(output.map(price => {
   if (price > prices[prices.length - 25]) {
     return 1;
@@ -31,7 +36,7 @@ const ys = tf.tensor2d(output.map(price => {
 
 (async () => {
   await model.fit(xs, ys, { epochs: 100 });
-  const prediction = model.predict(tf.tensor2d(input.slice(-1)[0], [1, input[0].length]));
+  const prediction = model.predict(tf.tensor2d(paddedInput.slice(-1)[0], [1, paddedInput[0].length]));
   const predictionValues = prediction.dataSync();
   console.log('Prediction:', predictionValues);
 })();
