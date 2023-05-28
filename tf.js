fd4bbce25a3761413056c0d4c33e72fbdae93863
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-28 19:20:46 +1000

    Update tf.js

--- tf.js
+++ tf.js
@@ -1,37 +1,43 @@
 const tf = require('@tensorflow/tfjs-node');
 const priceData = require('./price.json');
-const indRes = require('./indres.json');
+// Подготовка данных
+const data = priceData.map(price => parseFloat(price.close));
+const sequenceLength = 24;
+const sequences = [];
+for (let i = sequenceLength; i < data.length; i++) {
+sequences.push(data.slice(i - sequenceLength, i));
+}
+const inputs = sequences.slice(0, -1);
+const outputs = sequences.slice(1);
 
-const prices = priceData.map(candle => parseFloat(candle.close));
-const input = [
-  indRes.RSI.slice(0, -24),
-  indRes.EMA.slice(0, -24),
-  indRes.ROC.slice(0, -24),
-  //indRes.BollingerBands.slice(0, -24),
-  //indRes.BollingerBands.slice(band => band.lower).slice(0, -24),
-  indRes.OBV.slice(0, -24),
-  indRes.WMA.slice(0, -24),
-];
-const output = prices.slice(24);
-
+// Создание модели
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 64, inputShape: [input.length], activation: 'relu' }));
-model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
-model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
-
-const xs = tf.tensor2d(input.slice(-1)[0], [1, input[0].length]);
-const ys = tf.tensor2d(output.map(price => {
-  if (price > prices[prices.length - 25]) {
-    return 1;
-  } else {
-    return 0;
-  }
+model.add(tf.layers.lstm({
+units: 64,
+inputShape: [sequenceLength, 1],
+returnSequences: false
 }));
+model.add(tf.layers.dense({
+units: 1
+}));
+model.compile({
+optimizer: 'adam',
+loss: 'meanSquaredError'
+});
 
-(async () => {
-  await model.fit(xs, ys, { epochs: 100 });
-  const prediction = model.predict(tf.tensor2d(input.slice(-1)[0], [1, input[0].length]));
-  const predictionValues = prediction.dataSync();
-  console.log('Prediction:', predictionValues);
-})();
+// Обучение модели
+const xs = tf.tensor3d(inputs.map(sequence => sequence.map(value => [value])), [inputs.length, sequenceLength, 1]);
+const ys = tf.tensor3d(outputs.map(sequence => sequence.map(value => [value])), [outputs.length, sequenceLength, 1]);
+model.fit(xs, ys, {
+epochs: 100,
+batchSize: 32
+}).then(() => {
+// Тестирование модели
+const testSequence = data.slice(-sequenceLength);
+const testInput = tf.tensor3d(testSequence.map(value => [value]), [1, sequenceLength, 1]);
+const predictedSequence = model.predict(testInput).dataSync();
+console.log('Прогноз цены на 1 час:', predictedSequence[0].toFixed(2));
+console.log('Прогноз цены на 4 часа:', predictedSequence[3].toFixed(2));
+console.log('Прогноз цены на 12 часов:', predictedSequence[11].toFixed(2));
+console.log('Прогноз цены на 24 часа:', predictedSequence[23].toFixed(2));
+});
