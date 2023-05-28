628f4d6dbe183ea77037bfc8a83cff2e68b5861d
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-28 18:46:31 +1000

    Update tf.js

--- tf.js
+++ tf.js
@@ -1,142 +1,41 @@
-const tf = require('@tensorflow/tfjs-node');
-const fs = require('fs');
-const rawData = fs.readFileSync('price.json');
-const data = JSON.parse(rawData).map(candle => ({
-  open: parseFloat(candle.open),
-  high: parseFloat(candle.high),
-  low: parseFloat(candle.low),
-  close: parseFloat(candle.close),
-  volume: parseFloat(candle.volume),
-}));
+const tf = require('@tensorflow/tfjs');
+const priceData = require('./price.json');
+const indRes = require('./indres.json');
 
-const rawIndicators = fs.readFileSync('indres.json');
-let indicators;
-
-try {
-  const parsedData = JSON.parse(rawIndicators);
-  indicators = Object.keys(parsedData).map(key => ({
-    name: key,
-    values: parsedData[key].map(value => parseFloat(value)),
-  }));
-} catch (error) {
-  throw new Error('Indicators data is not valid JSON');
-}
-
-if (!Array.isArray(indicators)) {
-  throw new Error('Indicators data is not an array');
-}
-// Преобразование даты и значения индикатора
-const cleanIndicators = indicators.map(item => ({
-  date: new Date(item.date),
-  value: parseFloat(item.value),
-}));
-
-// Замена NaN на среднее значение по столбцу
-const values = cleanIndicators.map(item => item.value).filter(value => !isNaN(value));
-const meanValue = values.reduce((sum, value) => sum + value, 0) / values.length;
-
-const cleanData = cleanIndicators.map(item => ({
-  date: item.date,
-  value: isNaN(item.value) ? meanValue : item.value,
-}));
-
-// Объединение данных
-const mergedData = data.map((candle, index) => ({
-  ...candle,
-  ...indicators[index],
-}));
-
-// Подготовка данных
-const prices = mergedData.map(candle => candle.close);
-const minPrice = Math.min(...prices);
-const maxPrice = Math.max(...prices);
-const normalizedPrices = prices.map(price => (price - minPrice) / (maxPrice - minPrice));
-
-const windowSize = 720;
-const input = [];
-const output = [];
-
-// Дополнительный код для формирования входных и выходных данных
-
-
-
-for (let i = 0; i < normalizedPrices.length - windowSize; i++) {
-  const window = normalizedPrices.slice(i, i + windowSize);
-  const targetIndex = i + windowSize;
-
-  input.push([
-    ...window,
-    mergedData[targetIndex].RSI,
-    mergedData[targetIndex].EMA,
-    mergedData[targetIndex].ROC,
-    mergedData[targetIndex].BollingerBands,
-    mergedData[targetIndex].OBV,
-    mergedData[targetIndex].PSAR,
-    mergedData[targetIndex].WMA,
-  ]);
-
-  const futurePrice1h = normalizedPrices[targetIndex + 1];
-  const futurePrice4h = normalizedPrices[targetIndex + 4];
-  const futurePrice12h = normalizedPrices[targetIndex + 12];
-  const futurePrice24h = normalizedPrices[targetIndex + 24];
-
-  output.push([
-    futurePrice1h,
-    futurePrice4h,
-    futurePrice12h,
-    futurePrice24h,
-  ]);
-}
-
-// Обучение нейросети
-
-
+const prices = priceData.map(candle => parseFloat(candle.close));
+const input = [
+  indRes.rsi.slice(0, -24),
+  indRes.ema.slice(0, -24),
+  indRes.roc.slice(0, -24),
+  indRes.bb.map(band => band.upper).slice(0, -24),
+  indRes.bb.map(band => band.lower).slice(0, -24),
+  indRes.obv.slice(0, -24),
+  indRes.wma.slice(0, -24),
+];
+const output = prices.slice(24);
 
 const model = tf.sequential();
-
-model.add(tf.layers.dense({
-  inputShape: [windowSize + 7],
-  units: 64,
-  activation: 'relu',
-}));
-
-model.add(tf.layers.dense({
-  units: 4,
-  activation: 'linear',
-}));
-
-model.compile({
-  optimizer: tf.train.adam(),
-  loss: 'meanSquaredError',
-});
+model.add(tf.layers.dense({ units: 64, inputShape: [input.length], activation: 'relu' }));
+model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 4, activation: 'softmax' }));
+model.compile({ optimizer: 'adam', loss: 'categoricalCrossentropy', metrics: ['accuracy'] });
 
 const xs = tf.tensor2d(input);
-const ys = tf.tensor2d(output);
+const ys = tf.oneHot(tf.tensor1d(output.map(price => {
+  if (price > prices[prices.length - 25]) {
+    return 0;
+  } else if (price > prices[prices.length - 25] * 0.995) {
+    return 1;
+  } else if (price > prices[prices.length - 25] * 0.99) {
+    return 2;
+  } else {
+    return 3;
+  }
+}), 'int32'), 4);
 
-model.fit(xs, ys, {
-  epochs: 100,
-  shuffle: true,
-  validationSplit: 0.2,
-}).then(() => {
-  // Получение прогнозов
-  const lastWindow = normalizedPrices.slice(-windowSize);
-  const lastIndicators = mergedData.slice(-1)[0];
-
-  const input = [
-    ...lastWindow,
-    lastIndicators.RSI,
-    lastIndicators.EMA,
-    lastIndicators.ROC,
-    lastIndicators.BollingerBands,
-    lastIndicators.OBV,
-    lastIndicators.PSAR,
-    lastIndicators.WMA,
-  ];
-
-  const prediction = model.predict(tf.tensor2d([input]));
-  const predictionData = prediction.dataSync();
-
-  const denormalizedPrediction = predictionData.map(price => price * (maxPrice - minPrice) + minPrice);
-
-  console.log('Прогноз цены на следующие 1, 4, 12 и 24 часа:', denormalizedPrediction);
-});
+(async () => {
+  await model.fit(xs, ys, { epochs: 100 });
+  const prediction = model.predict(tf.tensor2d(input.slice(-1)));
+  const predictionValues = prediction.dataSync();
+  console.log('Prediction:', predictionValues);
+})();
