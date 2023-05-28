50a675e06ea22def3c3f7518b27a66813dc34629
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-28 12:45:55 +1000

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
-const chatId = '-1001979484873';
+const chatId = '-1001889085120';
 
 const bot = new TelegramBot(token, { polling: false });
 
