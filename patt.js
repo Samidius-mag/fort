42521973a334b8514f7b7950b586e4ca3e612244
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-29 09:58:29 +1000

    Update patt.js

--- patt.js
+++ patt.js
@@ -14,14 +14,14 @@ const isBullishHammer = bodyLength < upperShadowLength &&
 
 console.log(`Is Bullish Hammer: ${isBullishHammer}`);
 
-const candle = data[data.length - 2];
+const candle1 = data[data.length - 2];
 
-const bodyLength = Math.abs(candle.open - candle.close);
-const upperShadowLength = candle.high - Math.max(candle.open, candle.close);
-const lowerShadowLength = Math.min(candle.open, candle.close) - candle.low;
+const bodyLength = Math.abs(candle1.open - candle1.close);
+const upperShadowLength = candle1.high - Math.max(candle1.open, candle1.close);
+const lowerShadowLength = Math.min(candle1.open, candle1.close) - candle1.low;
 
 const isBearishHammer = bodyLength < lowerShadowLength &&
-  bodyLength <= candle.high / 3 &&
+  bodyLength <= candle1.high / 3 &&
   upperShadowLength <= bodyLength / 2;
 
 console.log(`Is Bearish Hammer: ${isBearishHammer}`);
