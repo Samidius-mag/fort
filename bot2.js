cf26c65894c674974b8eb02ed770517291705dc7
Samidius <sdirt5895@gmail.com>
2023-05-30 09:08:47 +1000

    dawd

--- bot2.js
+++ bot2.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
-const chatId = '-1001536433459';
+const chatId = '-1001979484873';
 
 const bot = new TelegramBot(token, { polling: false });
 
@@ -20,5 +20,26 @@ exec('node logic2.js', (error, stdout, stderr) => {
   const message = lines.join('\n');
 
   sendMessage(message);
+  console.log('Отправлено2');
+});
+
+const chatId2 = '-1001536433459';
+
+const bot2 = new TelegramBot(token, { polling: false });
+
+const sendMessage2 = (message) => {
+  bot2.sendMessage2(chatId2, message);
+};
+
+exec('node logic2.js', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  const lines = stdout.trim().split('\n');
+  const message = lines.join('\n');
+
+  sendMessage2(message);
   console.log('Отправлено2');
 });
--- index.js
+++ index.js
@@ -114,6 +114,22 @@ const sendToBot4 = () => {
     });
   };
 
+  const sendToBot7 = () => {
+    const child = spawn('node', ['bot2.js']);
+  
+    child.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  
+    child.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  
+    child.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  };
+
 setInterval(loadPrice, 10000);
 setInterval(sendToBot, 50000);
 setInterval(sendToBot2, 7000);
@@ -121,3 +137,4 @@ setInterval(sendToBot3, 310000);
 setInterval(sendToBot4, 350000);
 setInterval(sendToBot5, 400000);
 setInterval(sendToBot6, 500000);
+setInterval(sendToBot7, 300000);
