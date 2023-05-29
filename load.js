e1623926075c56f21e15a05b7b0de9c1896f60a7
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-29 18:46:09 +1000

    Update load.js

--- load.js
+++ load.js
@@ -50,3 +50,78 @@ axios.get(url)
   .catch(error => {
     console.error(error);
   });
+
+const url2 = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=24h&limit=5';
+
+axios.get(url)
+  .then(response => {
+    const data = response.data.map(candle => ({
+      time: candle[0],
+      open: candle[1],
+      high: candle[2],
+      low: candle[3],
+      close: candle[4],
+      volume: candle[5],
+      closeTime: candle[6],
+      quoteAssetVolume: candle[7],
+      numberOfTrades: candle[8],
+      takerBuyBaseAssetVolume: candle[9],
+      takerBuyQuoteAssetVolume: candle[10],
+    }));
+
+    fs.writeFileSync('price24.json', JSON.stringify(data));
+    console.log('Data saved to price24.json');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+const url3 = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=15m&limit=5';
+
+axios.get(url)
+  .then(response => {
+    const data = response.data.map(candle => ({
+      time: candle[0],
+      open: candle[1],
+      high: candle[2],
+      low: candle[3],
+      close: candle[4],
+      volume: candle[5],
+      closeTime: candle[6],
+      quoteAssetVolume: candle[7],
+      numberOfTrades: candle[8],
+      takerBuyBaseAssetVolume: candle[9],
+      takerBuyQuoteAssetVolume: candle[10],
+    }));
+
+    fs.writeFileSync('price15.json', JSON.stringify(data));
+    console.log('Data saved to price15.json');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+const url4 = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=5m&limit=5';
+
+axios.get(url)
+  .then(response => {
+    const data = response.data.map(candle => ({
+      time: candle[0],
+      open: candle[1],
+      high: candle[2],
+      low: candle[3],
+      close: candle[4],
+      volume: candle[5],
+      closeTime: candle[6],
+      quoteAssetVolume: candle[7],
+      numberOfTrades: candle[8],
+      takerBuyBaseAssetVolume: candle[9],
+      takerBuyQuoteAssetVolume: candle[10],
+    }));
+
+    fs.writeFileSync('price5.json', JSON.stringify(data));
+    console.log('Data saved to price5.json');
+  })
+  .catch(error => {
+    console.error(error);
+  });
