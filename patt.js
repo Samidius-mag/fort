160633a8e61c8ba7a0e235f40560445e063a5ac9
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-29 11:29:57 +1000

    Update patt.js

--- patt.js
+++ patt.js
@@ -26,59 +26,59 @@ const isBearishHammer = lastCandle1.close < lastCandle1.open &&
 console.log(`Is Bearish Hammer: ${isBearishHammer} ВНИЗ`);
 
 //Доджи
-const candle2 = data[data.length - 2];
-const bodyLength2 = Math.abs(candle.open - candle.close);
-const upperShadowLength2 = candle.high - Math.max(candle.open, candle.close);
-const lowerShadowLength2 = Math.min(candle.open, candle.close) - candle.low;
-const isDoji = bodyLength2 <= candle2.high * 0.1 &&
-  upperShadowLength2 <= candle2.high * 0.1 &&
-  lowerShadowLength2 <= candle2.high * 0.1;
+const candle = data[data.length - 2];
+const bodyLength = Math.abs(candle.open - candle.close);
+const upperShadowLength = candle.high - Math.max(candle.open, candle.close);
+const lowerShadowLength = Math.min(candle.open, candle.close) - candle.low;
+const isDoji = bodyLength <= candle.high * 0.1 &&
+  upperShadowLength <= candle.high * 0.1 &&
+  lowerShadowLength <= candle.high * 0.1;
 console.log(`Is Doji: ${isDoji} ЛОВУШКА`);
 
 //Завеса из облаков
-const prevCandle = data[data.length - 3];
-const lastCandle = data[data.length - 2];
-const avgClose = (prevCandle.close + lastCandle.close) / 2;
-const prevBodyLength = Math.abs(prevCandle.open - prevCandle.close);
-const lastBodyLength = Math.abs(lastCandle.open - lastCandle.close);
-const isDarkCloudCover = lastCandle.close < avgClose &&
-  lastCandle.open > prevCandle.close &&
-  lastCandle.close < prevCandle.open &&
-  lastBodyLength >= prevBodyLength * 0.5;
+const prevCandle2 = data[data.length - 3];
+const lastCandle2 = data[data.length - 2];
+const avgClose2 = (prevCandle.close + lastCandle.close) / 2;
+const prevBodyLength2 = Math.abs(prevCandle.open - prevCandle.close);
+const lastBodyLength2 = Math.abs(lastCandle.open - lastCandle.close);
+const isDarkCloudCover2 = lastCandle2.close < avgClose2 &&
+  lastCandle2.open > prevCandle2.close &&
+  lastCandle2.close < prevCandle2.open &&
+  lastBodyLength2 >= prevBodyLength2 * 0.5;
 console.log(`Is Dark Cloud Cover: ${isDarkCloudCover} ВНИЗ`);
 
 // Поглощение МЕДВЕЖЬЕ
-const prevCandle1 = data[data.length - 3];
-const lastCandle1 = data[data.length - 2];
-const prevBodyLength1 = Math.abs(prevCandle.open - prevCandle.close);
-const lastBodyLength1 = Math.abs(lastCandle.open - lastCandle.close);
-const isBearishEngulfing = lastCandle.close < prevCandle.open &&
-  lastCandle1.open > prevCandle1.close &&
-  lastBodyLength1 > prevBodyLength1 &&
-  lastCandle1.high <= lastCandle1.close &&
-  lastCandle1.low >= lastCandle1.open;
-console.log(`Is Bearish Engulfing Pattern: ${isBearishEngulfing} ВНИЗ`);
-
-// Поглощение БЫЧЬЕ
-const prevCandle2 = data[data.length - 3];
-const lastCandle2 = data[data.length - 2];
-const prevBodyLength2 = Math.abs(prevCandle.open - prevCandle.close);
-const lastBodyLength2 = Math.abs(lastCandle.open - lastCandle.close);
-const isBullishEngulfing = lastCandle.close > prevCandle.open &&
-  lastCandle2.open < prevCandle2.close &&
-  lastBodyLength2 > prevBodyLength2 &&
-  lastCandle2.high <= lastCandle2.open &&
-  lastCandle2.low >= lastCandle2.close;
-console.log(`Is Bullish Engulfing Pattern: ${isBullishEngulfing} ВВЕРХ`);
-
-//Пронизывающая 
 const prevCandle3 = data[data.length - 3];
 const lastCandle3 = data[data.length - 2];
 const prevBodyLength3 = Math.abs(prevCandle.open - prevCandle.close);
-const prevShadowLength3 = Math.abs(prevCandle.high - prevCandle.low);
-const prevMidPrice3 = (prevCandle.open + prevCandle.close) / 2;
 const lastBodyLength3 = Math.abs(lastCandle.open - lastCandle.close);
-const isPiercingLine = lastCandle3.close > prevCandle3.open &&
+const isBearishEngulfing = lastCandle3.close < prevCandle3.open &&
+  lastCandle3.open > prevCandle3.close &&
+  lastBodyLength3 > prevBodyLength3 &&
+  lastCandle3.high <= lastCandle3.close &&
+  lastCandle3.low >= lastCandle3.open;
+console.log(`Is Bearish Engulfing Pattern: ${isBearishEngulfing} ВНИЗ`);
+
+// Поглощение БЫЧЬЕ
+const prevCandle4 = data[data.length - 3];
+const lastCandle4 = data[data.length - 2];
+const prevBodyLength4 = Math.abs(prevCandle.open - prevCandle.close);
+const lastBodyLength4 = Math.abs(lastCandle.open - lastCandle.close);
+const isBullishEngulfing = lastCandle4.close > prevCandle4.open &&
+  lastCandle4.open < prevCandle4.close &&
+  lastBodyLength4 > prevBodyLength4 &&
+  lastCandle4.high <= lastCandle4.open &&
+  lastCandle4.low >= lastCandle4.close;
+console.log(`Is Bullish Engulfing Pattern: ${isBullishEngulfing} ВВЕРХ`);
+
+//Пронизывающая 
+const prevCandle5= data[data.length - 3];
+const lastCandle5= data[data.length - 2];
+const prevBodyLength5= Math.abs(prevCandle.open - prevCandle.close);
+const prevShadowLength5 =Math.abs(prevCandle.high - prevCandle.low);
+const prevMidPrice5= (prevCandle.open + prevCandle.close) / 2;
+const lastBodyLength5= Math.abs(lastCandle.open - lastCandle.close);
+const isPiercingLine = lastCandle5close > prevCandle3.open &&
   lastCandle3.open < prevCandle3.close &&
   lastCandle3.close > prevMidPrice3 &&
   lastBodyLength3 > prevBodyLength3 &&
