e241dc89fc33f1391195aecb1e96acf0bbd56cb8
Samidius <sdirt5895@gmail.com>
2023-05-31 10:06:41 +1000

    вцфцв

--- tf.js
+++ tf.js
@@ -2,12 +2,25 @@ const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
-const candles = data.slice(0, -2);
+
+async function trainModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ inputShape: [4], units: 5, activation: 'sigmoid' }));
+  model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+  model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy' });
+
+  const xs = tf.tensor2d(data.slice(0, -1).map(candle => [candle.open, candle.high, candle.low, candle.close]));
+  const ys = tf.tensor2d(data.slice(1).map(candle => [candle.close > candle.open ? 1 : 0]));
+
+  await model.fit(xs, ys, { epochs: 100 });
+
+  await model.save('model.json');
+}
 
 async function predictNextCandleMovement() {
   const model = await tf.loadLayersModel('model.json');
 
-  const lastCandle = candles[candles.length - 1];
+  const lastCandle = data[data.length - 2];
   const input = tf.tensor2d([[lastCandle.open, lastCandle.high, lastCandle.low, lastCandle.close]]);
 
   const output = model.predict(input);
@@ -20,4 +33,4 @@ async function predictNextCandleMovement() {
   }
 }
 
-predictNextCandleMovement();
+trainModel().then(predictNextCandleMovement);
