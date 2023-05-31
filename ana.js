a6ea59be26da5a61222e89d09c20bb3c6a6e3646
Samidius <sdirt5895@gmail.com>
2023-05-31 16:17:40 +1000

    wdawd

--- ana.js
+++ ana.js
@@ -5,6 +5,8 @@ const analysisData = JSON.parse(fs.readFileSync('anres.json'));
 
 const averageVolume = analysisData.averageVolume;
 
+const result = [];
+
 for (let i = 1; i < priceData.length - 1; i++) { // не учитываем первую и последнюю свечу
   const currentCandle = priceData[i];
   const prevCandle = priceData[i - 1];
@@ -12,12 +14,28 @@ for (let i = 1; i < priceData.length - 1; i++) { // не учитываем пе
   const prevVolume = parseFloat(prevCandle.volume);
   if (!isNaN(currentVolume) && !isNaN(prevVolume)) {
     const volumeRatio = currentVolume / prevVolume;
+    const date = new Date(currentCandle.time);
+    const dateString = `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}${date.getMinutes().toString().padStart(2, '0')}${date.getSeconds().toString().padStart(2, '0')}`;
     if (volumeRatio > 1.5 && currentVolume > averageVolume) {
-      console.log(`Market is bullish at ${currentCandle.time}`);
+      result.push({
+        date: dateString,
+        volume: currentVolume,
+        direction: 'bullish'
+      });
     } else if (volumeRatio < 0.5 && currentVolume < averageVolume) {
-      console.log(`Market is bearish at ${currentCandle.time}`);
+      result.push({
+        date: dateString,
+        volume: currentVolume,
+        direction: 'bearish'
+      });
     } else {
-      console.log(`Market is stable at ${currentCandle.time}`);
+      result.push({
+        date: dateString,
+        volume: currentVolume,
+        direction: 'stable'
+      });
     }
   }
 }
+
+fs.writeFileSync('vol.json', JSON.stringify(result));
