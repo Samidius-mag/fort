e4a3822c0d4ec1edf851a52cc715fd61747aca7f
Samidius <sdirt5895@gmail.com>
2023-05-31 10:55:18 +1000

    151654

--- tf.js
+++ tf.js
@@ -1,57 +1,64 @@
-const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
+const tf = require('@tensorflow/tfjs');
+const tfvis = require('@tensorflow/tfjs-vis');
 
+const data = JSON.parse(fs.readFileSync('price.json'));
 
-// Загрузка данных из файла price.json
-const rawData = fs.readFileSync('price.json');
-const data = JSON.parse(rawData).map(candle => ({
-  open: parseFloat(candle.open),
-  high: parseFloat(candle.high),
-  low: parseFloat(candle.low),
-  close: parseFloat(candle.close),
-  volume: parseFloat(candle.volume),
-}));
-
-// Преобразование данных в тензоры
-const inputTensor = tf.tensor(data.map(candle => [
+const input = data.slice(0, -1).map(candle => [
   candle.open,
   candle.high,
   candle.low,
   candle.close,
   candle.volume,
-]));
-const outputTensor = tf.tensor(data.map(candle => [
-  candle.close,
-]));
+]);
 
-async function trainModel() {
-  const model = tf.sequential();
-  model.add(tf.layers.dense({ inputShape: [4], units: 5, activation: 'sigmoid' }));
-  model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-  model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy' });
+const output = data.slice(1).map(candle => [
+  candle.open > candle.close ? 0 : 1,
+]);
 
-  const xs = tf.tensor2d(data.slice(0, -1).map(candle => [candle.open, candle.high, candle.low, candle.close]));
-  const ys = tf.tensor2d(data.slice(1).map(candle => [candle.close > candle.open ? 1 : 0]));
+const model = tf.sequential();
 
-  await model.fit(xs, ys, { epochs: 100 });
+model.add(tf.layers.dense({
+  inputShape: [5],
+  activation: 'relu',
+  units: 32,
+}));
 
-  await model.save('root/fort/model.json');
-}
+model.add(tf.layers.dense({
+  activation: 'sigmoid',
+  units: 1,
+}));
 
-async function predictNextCandleMovement() {
-  const model = await tf.loadLayersModel('root/fort/model.json');
+model.compile({
+  optimizer: tf.train.adam(),
+  loss: 'binaryCrossentropy',
+  metrics: ['accuracy'],
+});
 
-  const lastCandle = data[data.length - 2];
-  const input = tf.tensor2d([[lastCandle.open, lastCandle.high, lastCandle.low, lastCandle.close]]);
+const batchSize = 32;
+const epochs = 50;
 
-  const output = model.predict(input);
-  const prediction = Array.from(output.dataSync())[0];
+model.fit(tf.tensor2d(input), tf.tensor2d(output), {
+  batchSize,
+  epochs,
+  shuffle: true,
+  callbacks: tfvis.show.fitCallbacks(
+    { name: 'Training Performance' },
+    ['loss', 'acc'],
+    { height: 200, callbacks: ['onEpochEnd'] },
+  ),
+}).then(() => {
+  const lastCandle = data[data.length - 1];
+  const inputTensor = tf.tensor2d([[
+    lastCandle.open,
+    lastCandle.high,
+    lastCandle.low,
+    lastCandle.close,
+    lastCandle.volume,
+  ]]);
 
-  if (prediction > 0.5) {
-    console.log('Следующая свеча движется вверх');
-  } else {
-    console.log('Следующая свеча движется вниз');
-  }
-}
+  const prediction = model.predict(inputTensor);
+  const direction = prediction.dataSync()[0] > 0.5 ? 'up' : 'down';
 
-trainModel().then(predictNextCandleMovement);
+  console.log(`Next candle will go ${direction}`);
+});
