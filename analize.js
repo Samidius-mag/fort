7bf224d79ff89fe4cfbbefa4e0b35faa65661b3c
Samidius <sdirt5895@gmail.com>
2023-05-31 15:49:40 +1000

    вфцф

--- analize.js
+++ analize.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('price.json'));
 
 let totalVolume = 0;
-for (let i = 0; i < data.length; i++) {
+for (let i = 0; i < data.length - 1; i++) { // не учитываем последнюю свечу
   const candle = data[i];
   totalVolume += candle.volume;
 }
 
-const averageVolume = totalVolume / data.length;
+const averageVolume = totalVolume / (data.length - 1); // делим на количество свечей минус одна
 console.log(`Average volume: ${averageVolume}`);
