e8384d114fc8a719d6d4f406d4f99341a9e54054
Samidius <sdirt5895@gmail.com>
2023-05-31 17:46:10 +1000

    1545

--- analize.js
+++ analize.js
@@ -8,9 +8,21 @@ fs.readFile('indres.json', (err, data) => {
   const indRes = JSON.parse(data);
   const obv = indRes.OBV;
 
-  // Считаем среднее значение объема
-  const sum = obv.reduce((acc, val) => acc + val, 0);
-  const avg = sum / obv.length;
+  // Находим максимальное и минимальное значение объема
+  const maxVolume = Math.max(...obv);
+  const minVolume = Math.min(...obv);
 
-  console.log(`Средний объем: ${avg}`);
+  // Вычисляем среднее значение объема
+  const avgVolume = (maxVolume + minVolume) / 2;
+
+  // Определяем, насколько текущий объем отличается от среднего значения
+  const currentVolume = obv[obv.length - 1];
+  const volumeDiff = currentVolume - avgVolume;
+
+  // Если текущий объем больше среднего значения, то это может свидетельствовать о продолжении тренда
+  if (volumeDiff > 0) {
+    console.log(`Текущий объем (${currentVolume}) больше среднего значения (${avgVolume}). Возможно, тренд продолжится.`);
+  } else {
+    console.log(`Текущий объем (${currentVolume}) меньше или равен среднему значению (${avgVolume}). Возможно, тренд завершится.`);
+  }
 });
