e213905be264d78e6c17d2f907d231c8dcca7c85
Samidius <sdirt5895@gmail.com>
2023-06-01 07:31:43 +1000

    wda

---
+++ canal.js
@@ -0,0 +1,62 @@
+const _ = require('lodash');
+const regression = require('regression');
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('price.json'));
+
+// Получаем последние 10 часовых свечей
+const lastCandles = data.slice(-10);
+
+// Получаем массив цен закрытия свечей
+const prices = lastCandles.map(candle => parseFloat(candle.close));
+
+// Получаем массив точек минимумов и максимумов
+const points = [];
+for (let i = 1; i < prices.length - 1; i++) {
+  if (prices[i] > prices[i - 1] && prices[i] > prices[i + 1]) {
+    points.push([i, prices[i]]);
+  } else if (prices[i] < prices[i - 1] && prices[i] < prices[i + 1]) {
+    points.push([i, prices[i]]);
+  }
+}
+
+// Вычисляем линейную регрессию через точки минимумов и максимумов
+const result = regression.linear(points);
+
+// Получаем коэффициенты линейной регрессии
+const slope = result.equation[0];
+const intercept = result.equation[1];
+
+// Вычисляем цены на линии сопротивления и поддержки
+const resistancePrice = slope * prices.length + intercept;
+const supportPrice = slope * (prices.length - 1) + intercept;
+
+// Определяем среднюю линию канала
+const middlePrice = (resistancePrice + supportPrice) / 2;
+
+// Получаем точки на линии сопротивления и поддержки
+const resistancePoints = regression.getPoints(points, result).map(point => [point[0] + prices.length - points.length, point[1]]);
+const supportPoints = regression.getPoints(points, result).map(point => [point[0] + prices.length - points.length - 1, point[1]]);
+
+// Определяем границы канала
+const upperBound = resistancePoints[0][1];
+const lowerBound = supportPoints[0][1];
+
+// Определяем текущую цену
+const currentPrice = parseFloat(lastCandles[9].close);
+
+// Проверяем, вышла ли цена за границы канала
+let message = '';
+if (currentPrice > upperBound) {
+  message = 'Price is above the channel';
+} else if (currentPrice < lowerBound) {
+  message = 'Price is below the channel';
+}
+
+console.log(`Resistance: ${resistancePrice}`);
+console.log(`Support: ${supportPrice}`);
+console.log(`Middle: ${middlePrice}`);
+console.log(`Upper bound: ${upperBound}`);
+console.log(`Lower bound: ${lowerBound}`);
+console.log(`Current price: ${currentPrice}`);
+console.log(`Message: ${message}`);
