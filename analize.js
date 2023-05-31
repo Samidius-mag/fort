2a6796bd423079e89e7680ff467ea788436aed90
Samidius <sdirt5895@gmail.com>
2023-05-31 15:47:02 +1000

    цффцфв

--- analize.js
+++ analize.js
@@ -2,37 +2,11 @@ const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
 
-const trendLength = 3; // длина тренда в свечах
-const trendVolumeThreshold = 1000; // пороговое значение объема для продолжения тренда
-
-let trendVolumes = [];
-let trendVolumesSum = 0;
-let trendVolumesCount = 0;
-
-let results = []; // объявляем переменную results
-
+let totalVolume = 0;
 for (let i = 0; i < data.length; i++) {
   const candle = data[i];
-
-  if (i < trendLength) {
-    trendVolumes.push(candle.volume);
-    trendVolumesSum += candle.volume;
-    trendVolumesCount++;
-  } else {
-    const trendVolume = trendVolumesSum / trendVolumesCount;
-
-    if (trendVolume > trendVolumeThreshold) {
-      console.log(`Trend continues with average volume ${trendVolume}`);
-      results.push({ trend: 'continue', volume: trendVolume });
-    } else {
-      console.log(`Trend ends with average volume ${trendVolume}`);
-      results.push({ trend: 'end', volume: trendVolume });
-    }
-
-    trendVolumesSum -= trendVolumes.shift();
-    trendVolumes.push(candle.volume);
-    trendVolumesSum += candle.volume;
-  }
+  totalVolume += candle.volume;
 }
-console.log(`Trend continues with average volume ${trendVolumes}`);
-fs.writeFileSync('anres.json', JSON.stringify(results));
+
+const averageVolume = totalVolume / data.length;
+console.log(`Average volume: ${averageVolume}`);
