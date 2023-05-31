6210e1ba7cdfbcb3e5e749d499adfa7887c3cdcf
Samidius <sdirt5895@gmail.com>
2023-05-31 15:51:03 +1000

    вфцвфцв

--- analize.js
+++ analize.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('price.json'));
 
 let totalVolume = 0;
+let validCandlesCount = 0;
 for (let i = 0; i < data.length - 1; i++) { // не учитываем последнюю свечу
   const candle = data[i];
-  totalVolume += candle.volume;
+  if (typeof candle.volume === 'number' && !isNaN(candle.volume)) {
+    totalVolume += candle.volume;
+    validCandlesCount++;
+  }
 }
 
-const averageVolume = totalVolume / (data.length - 1); // делим на количество свечей минус одна
+const averageVolume = totalVolume / validCandlesCount;
 console.log(`Average volume: ${averageVolume}`);
