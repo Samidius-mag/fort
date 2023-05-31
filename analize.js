a64be173622914ede316beeb4521e1f511328846
Samidius <sdirt5895@gmail.com>
2023-05-31 15:53:53 +1000

    цфвфцв

--- analize.js
+++ analize.js
@@ -6,8 +6,9 @@ let totalVolume = 0;
 let validCandlesCount = 0;
 for (let i = 0; i < data.length - 1; i++) { // не учитываем последнюю свечу
   const candle = data[i];
-  if (typeof candle.volume === 'number' && !isNaN(candle.volume)) {
-    totalVolume += candle.volume;
+  const volume = parseFloat(candle.volume);
+  if (!isNaN(volume)) {
+    totalVolume += volume;
     validCandlesCount++;
   }
 }
