09b4d39fde36e6bcbc0c61d4c2eb93b8727650a7
Samidius <sdirt5895@gmail.com>
2023-06-03 09:14:49 +1000

    dawdawddd

--- canaliz3.js
+++ canaliz3.js
@@ -1,4 +1,5 @@
 const data = require('./price.json');
+const fs = require('fs');
 
 const lastCandle = data[data.length - 1];
 const prevCandle = data[data.length - 2];
@@ -34,9 +35,17 @@ const nextLow = lastLow - (lastHigh - lastLow);
 
 channel.push({ high: nextHigh, low: nextLow });
 
-//console.log(`Price channel for the next day:`, channel);
+console.log(`Price channel for the next day:`, channel);
 
 const max = Math.max(...channel.map(candle => candle.high));
 const min = Math.min(...channel.map(candle => candle.low));
 
 console.log(`Maximum: ${max}, Minimum: ${min}`);
+
+fs.writeFile('canalizres.json', JSON.stringify({ channel, max, min }), err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Data has been saved to canalizres.json');
+});
