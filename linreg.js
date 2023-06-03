203ff1f333594e04a22e009889c3347c76788227
Samidius <sdirt5895@gmail.com>
2023-06-03 18:30:41 +1000

    2312354

--- linreg.js
+++ linreg.js
@@ -1,6 +1,6 @@
 const math = require('mathjs');
 const data = require('./price.json');
-
+/*
 const prices = data.map(candle => candle.close);
 const lows = data.map(candle => candle.low);
 const highs = data.map(candle => candle.high);
@@ -15,3 +15,24 @@ console.log(`Mean price: ${meanPrice}`);
 console.log(`Mean low: ${meanLow}`);
 console.log(`Mean high: ${meanHigh}`);
 console.log(`Mean volume: ${meanVolume}`);
+*/
+
+const prices = data.map(candle => candle.close);
+const dates = data.map(candle => new Date(candle.date));
+
+const meanPrice = math.mean(prices);
+const meanDate = math.mean(dates);
+
+const n = prices.length;
+const sumX = math.sum(dates);
+const sumY = math.sum(prices);
+const sumXY = math.sum(math.dotMultiply(dates, prices));
+const sumX2 = math.sum(math.dotPow(dates, 2));
+
+const b = (n * sumXY - sumX * sumY) / (n * sumX2 - math.pow(sumX, 2));
+const a = meanPrice - b * meanDate;
+
+const nextDate = new Date();
+const nextPrice = a + b * nextDate.getTime();
+
+console.log(`Next price (linear regression): ${nextPrice}`);
