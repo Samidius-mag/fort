b7b15f361f83425df1ec131edadd9d3b6819783a
Samidius <sdirt5895@gmail.com>
2023-06-03 18:40:49 +1000

    21348

--- linreg.js
+++ linreg.js
@@ -17,22 +17,24 @@ console.log(`Mean high: ${meanHigh}`);
 console.log(`Mean volume: ${meanVolume}`);
 */
 
-const prices = data.map(candle => candle.close);
-const dates = data.map(candle => new Date(candle.date).getTime()); // использование метода getTime()
 
-const meanPrice = math.mean(prices);
-const meanDate = math.mean(dates);
 
-const n = prices.length;
-const sumX = math.sum(dates);
-const sumY = math.sum(prices);
-const sumXY = math.sum(math.dotMultiply(dates, prices));
-const sumX2 = math.sum(math.dotPow(dates, 2));
+const x = data.map(candle => candle.close);
+const y = data.map(candle => candle.low);
+
+const n = x.length;
+const sumX = math.sum(x);
+const sumY = math.sum(y);
+const sumXY = math.sum(math.dotMultiply(x, y));
+const sumX2 = math.sum(math.dotPow(x, 2));
 
 const b = (n * sumXY - sumX * sumY) / (n * sumX2 - math.pow(sumX, 2));
-const a = meanPrice - b * meanDate;
+const a = (sumY - b * sumX) / n;
 
-const nextDate = new Date();
-const nextPrice = a + b * nextDate.getTime();
+console.log('a:', a); // отладочный вывод
+console.log('b:', b); // отладочный вывод
 
-console.log(`Next price (linear regression): ${nextPrice}`);
+const nextPrice = a + b * x[x.length - 1];
+
+console.log('Next price (linear regression):', nextPrice); // отладочный вывод
+
