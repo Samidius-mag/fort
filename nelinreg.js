b59d57b72fa0be0b010e0dd8967d5df6a6e428d2
Samidius <sdirt5895@gmail.com>
2023-06-03 18:47:32 +1000

    123484

--- nelinreg.js
+++ nelinreg.js
@@ -1,7 +1,6 @@
 const math = require('mathjs');
 const data = require('./price.json');
 
-/*
 const x = data.map(candle => candle.close);
 const y = data.map(candle => candle.low);
 
@@ -12,23 +11,27 @@ const sumXY = math.sum(math.dotMultiply(x, y));
 const sumX2 = math.sum(math.dotPow(x, 2));
 
 const b = (n * sumXY - sumX * sumY) / (n * sumX2 - math.pow(sumX, 2));
-const a = math.mean(y) - b * math.mean(x);
+const a = (sumY - b * sumX) / n;
 
-console.log(`a: ${a}`);
-console.log(`b: ${b}`);
-*/
-const x = data.map(candle => candle.close);
-const y = data.map(candle => candle.low);
+console.log('a:', a); // отладочный вывод
+console.log('b:', b); // отладочный вывод
 
-const n = x.length;
-const sumX = math.sum(x);
-const sumY = math.sum(y);
-const sumXY = math.sum(math.dotMultiply(x, y));
-const sumX2 = math.sum(math.dotPow(x, 2));
+const nextPrice = a + b * x[x.length - 1];
 
-const b = (n * sumXY - sumX * sumY) / (n * sumX2 - math.pow(sumX, 2));
-const a = math.mean(y) / math.pow(math.mean(x), b);
+console.log('Next price (linear regression):', nextPrice); // отладочный вывод
 
-const nextPrice = a * Math.pow(x[x.length - 1], b);
+const x1 = data.map(candle => candle.close);
+const y1 = data.map(candle => candle.low);
 
-console.log(`Next price (nonlinear regression): ${nextPrice}`);
+const n1 = x1.length;
+const sumX1 = math.sum(x1);
+const sumY1 = math.sum(y1);
+const sumXY1 = math.sum(math.dotMultiply(x1, y1));
+const sumX21 = math.sum(math.dotPow(x1, 2));
+
+const b1 = (n1 * sumXY1 - sumX1 * sumY1) / (n1 * sumX21 - math.pow(sumX1, 2));
+const a1 = math.mean(y1) / math.pow(math.mean(x), b1);
+
+const nextPrice1 = a1 * Math.pow(x[x.length - 1], b1);
+
+console.log(`Next price (nonlinear regression): ${nextPrice1}`);
