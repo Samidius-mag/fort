efe3c089eb9c665c4290584c6dad633c0cd867d1
Samidius <sdirt5895@gmail.com>
2023-06-03 16:32:00 +1000

    42341

--- nelinreg.js
+++ nelinreg.js
@@ -1,75 +1,42 @@
 const fs = require('fs');
-const math = require('mathjs');
+const _ = require('lodash');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
 
-const n = data.length;
+const sum = (arr) => arr.reduce((acc, val) => acc + val, 0);
+const mean = (arr) => sum(arr) / arr.length;
 
-let sumPrice = 0;
-let sumMin = 0;
-let sumMax = 0;
-let sumVolume = 0;
+const x = data.map(candle => [candle.open, candle.high, candle.low, candle.close, candle.volume]);
+const y = data.map(candle => [candle.open, candle.high, candle.low, candle.close, candle.volume]);
 
-for (let i = 0; i < n; i++) {
-  sumPrice += data[i].close;
-  sumMin += data[i].low;
-  sumMax += data[i].high;
-  sumVolume += data[i].volume;
-}
+const n = x.length;
+const k = x[0].length;
 
-const avgPrice = sumPrice / n;
-const avgMin = sumMin / n;
-const avgMax = sumMax / n;
-const avgVolume = sumVolume / n;
+const xT = _.zip(...x);
+const yT = _.zip(...y);
 
-function model(x, a, b, c, d) {
-  return a * Math.sin(b * x + c) + d;
-}
+const xTx = _.multiply(xT, x);
+const xTy = _.multiply(xT, y);
 
-const x = math.range(0, n, true).toArray();
-const yPrice = data.map(candle => candle.close);
-const yMin = data.map(candle => candle.low);
-const yMax = data.map(candle => candle.high);
-const yVolume = data.map(candle => candle.volume);
+const beta = _.multiply(_.invert(xTx), xTy);
 
-const optimizePrice = math.optimize(model, [1, 0.01, 0, avgPrice], x, yPrice);
-const optimizeMin = math.optimize(model, [1, 0.01, 0, avgMin], x, yMin);
-const optimizeMax = math.optimize(model, [1, 0.01, 0, avgMax], x, yMax);
-const optimizeVolume = math.optimize(model, [1, 0.01, 0, avgVolume], x, yVolume);
+const yHat = _.multiply(x, beta);
 
-const resultPrice = optimizePrice._data[0];
-const resultMin = optimizeMin._data[0];
-const resultMax = optimizeMax._data[0];
-const resultVolume = optimizeVolume._data[0];
+const residuals = _.subtract(y, yHat);
 
-const aPrice = resultPrice.coefficients[0];
-const bPrice = resultPrice.coefficients[1];
-const cPrice = resultPrice.coefficients[2];
-const dPrice = resultPrice.coefficients[3];
+const sse = sum(residuals.map(row => sum(row.map(val => val ** 2))));
+const sst = sum(y.map(row => sum(row.map(val => (val - mean(row)) ** 2))));
+const r2 = 1 - sse / sst;
 
-const aMin = resultMin.coefficients[0];
-const bMin = resultMin.coefficients[1];
-const cMin = resultMin.coefficients[2];
-const dMin = resultMin.coefficients[3];
+const avgOpen = mean(data.map(candle => candle.open));
+const avgHigh = mean(data.map(candle => candle.high));
+const avgLow = mean(data.map(candle => candle.low));
+const avgClose = mean(data.map(candle => candle.close));
+const avgVolume = mean(data.map(candle => candle.volume));
 
-const aMax = resultMax.coefficients[0];
-const bMax = resultMax.coefficients[1];
-const cMax = resultMax.coefficients[2];
-const dMax = resultMax.coefficients[3];
-
-const aVolume = resultVolume.coefficients[0];
-const bVolume = resultVolume.coefficients[1];
-const cVolume = resultVolume.coefficients[2];
-const dVolume = resultVolume.coefficients[3];
-
-const nextHour = n;
-const nextHourPrice = model(nextHour, aPrice, bPrice, cPrice, dPrice);
-const nextHourMin = model(nextHour, aMin, bMin, cMin, dMin);
-const nextHourMax = model(nextHour, aMax, bMax, cMax, dMax);
-const nextHourVolume = model(nextHour, aVolume, bVolume, cVolume, dVolume);
-
-console.log(`Прогноз на следующий час:
-Средняя цена: ${nextHourPrice}
-Минимальная цена: ${nextHourMin}
-Максимальная цена: ${nextHourMax}
-Объем торгов: ${nextHourVolume}`);
+console.log(`Average open: ${avgOpen}`);
+console.log(`Average high: ${avgHigh}`);
+console.log(`Average low: ${avgLow}`);
+console.log(`Average close: ${avgClose}`);
+console.log(`Average volume: ${avgVolume}`);
+console.log(`R-squared: ${r2}`);
