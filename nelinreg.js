a1d1531d006875d0ee5cff51c510bc53b80056da
Samidius <sdirt5895@gmail.com>
2023-06-03 17:05:51 +1000

    13231

--- nelinreg.js
+++ nelinreg.js
@@ -1,31 +1,29 @@
+const math = require('mathjs');
 const fs = require('fs');
-const fmin = require('fmin');
-const nelderMead = fmin.nelderMead;
+
 const data = JSON.parse(fs.readFileSync('price.json'));
 
-const x = data.map(candle => candle.time);
+// Создаем массивы для независимой и зависимой переменных
+const x = data.map(candle => candle.volume);
 const y = data.map(candle => candle.close);
-const f = (x, beta) => beta[0] + beta[1] * x + beta[2] * x * x;
-const beta0 = Math.random();
-const beta1 = Math.random();
-const beta2 = Math.random();
-const beta = [beta0, beta1, beta2];
-const error = (beta, x, y) => {
-    const yPredicted = x.map(xi => f(xi, beta));
-    const residuals = y.map((yi, i) => yi - yPredicted[i]);
-    const sumOfSquares = residuals.reduce((sum, residual) => sum + residual * residual, 0);
-    return sumOfSquares;
-  };
-  const optimize = (beta, x, y) => {
-    const options = { maxIterations: 1000 };
-    const result = nelderMead(error, beta, options, x, y);
-    return result.x;
-  };
 
-  const optimizedBeta = optimize(beta, x, y);
+// Определяем функцию, которую будем использовать для моделирования
+const f = (x, beta) => {
+  const [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10] = beta;
+  return b0 + b1 * x + b2 * x ** 2 + b3 * x ** 3 + b4 * x ** 4 + b5 * x ** 5 + b6 * x ** 6 + b7 * x ** 7 + b8 * x ** 8 + b9 * x ** 9 + b10 * x ** 10;
+};
 
-  const newX = [x[0], x[x.length - 1] + 3600 * 1000]; // новые значения времени (через час после последней свечи)
-const newY = newX.map(xi => f(xi, optimizedBeta)); // предсказанные значения цены закрытия
+// Определяем начальные значения для коэффициентов
+const initialBeta = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-console.log('Предсказанные значения:');
-console.log(newY);
+// Используем метод наименьших квадратов для определения коэффициентов
+const result = math.optimize((beta) => {
+  const yPredicted = x.map(x => f(x, beta));
+  const residuals = math.subtract(y, yPredicted);
+  const rss = math.sum(math.square(residuals));
+  return rss;
+}, { method: 'levenbergMarquardt', initialValues: initialBeta });
+
+// Выводим результаты
+console.log('Coefficients:', result.solution);
+console.log('R-squared:', math.round(result.residual / math.sum(math.square(math.subtract(y, math.mean(y)))), 4));
