8f5835856391c12d2d21bea3212393716370c2fd
Samidius <sdirt5895@gmail.com>
2023-06-03 16:56:25 +1000

    1321314

--- nelinreg.js
+++ nelinreg.js
@@ -1,42 +1,29 @@
 const fs = require('fs');
-const _ = require('lodash');
-
 const data = JSON.parse(fs.readFileSync('price.json'));
 
-const sum = (arr) => arr.reduce((acc, val) => acc + val, 0);
-const mean = (arr) => sum(arr) / arr.length;
+const x = data.map(candle => candle.time);
+const y = data.map(candle => candle.close);
+const f = (x, beta) => beta[0] + beta[1] * x + beta[2] * x * x;
+const beta0 = Math.random();
+const beta1 = Math.random();
+const beta2 = Math.random();
+const beta = [beta0, beta1, beta2];
+const error = (beta, x, y) => {
+    const yPredicted = x.map(xi => f(xi, beta));
+    const residuals = y.map((yi, i) => yi - yPredicted[i]);
+    const sumOfSquares = residuals.reduce((sum, residual) => sum + residual * residual, 0);
+    return sumOfSquares;
+  };
+  const optimize = (beta, x, y) => {
+    const options = { maxIterations: 1000 };
+    const result = nelderMead(error, beta, options, x, y);
+    return result.x;
+  };
 
-const x = data.map(candle => [candle.open, candle.high, candle.low, candle.close, candle.volume]);
-const y = data.map(candle => [candle.open, candle.high, candle.low, candle.close, candle.volume]);
+  const optimizedBeta = optimize(beta, x, y);
 
-const n = x.length;
-const k = x[0].length;
+  const newX = [x[0], x[x.length - 1] + 3600 * 1000]; // новые значения времени (через час после последней свечи)
+const newY = newX.map(xi => f(xi, optimizedBeta)); // предсказанные значения цены закрытия
 
-const xT = _.zip(...x);
-const yT = _.zip(...y);
-
-const xTx = _.multiply(xT, x);
-const xTy = _.multiply(xT, y);
-
-const beta = _.multiply(_.invert(xTx), xTy);
-
-const yHat = _.multiply(x, beta);
-
-const residuals = _.subtract(y, yHat);
-
-const sse = sum(residuals.map(row => sum(row.map(val => val ** 2))));
-const sst = sum(y.map(row => sum(row.map(val => (val - mean(row)) ** 2))));
-const r2 = 1 - sse / sst;
-
-const avgOpen = mean(data.map(candle => candle.open));
-const avgHigh = mean(data.map(candle => candle.high));
-const avgLow = mean(data.map(candle => candle.low));
-const avgClose = mean(data.map(candle => candle.close));
-const avgVolume = mean(data.map(candle => candle.volume));
-
-console.log(`Average open: ${avgOpen}`);
-console.log(`Average high: ${avgHigh}`);
-console.log(`Average low: ${avgLow}`);
-console.log(`Average close: ${avgClose}`);
-console.log(`Average volume: ${avgVolume}`);
-console.log(`R-squared: ${r2}`);
+console.log('Предсказанные значения:');
+console.log(newY);
