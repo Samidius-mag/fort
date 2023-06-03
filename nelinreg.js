f7ace218d68440b197bcfef2d8651777efeba966
Samidius <sdirt5895@gmail.com>
2023-06-03 16:00:00 +1000

    423425

--- nelinreg.js
+++ nelinreg.js
@@ -32,15 +32,15 @@ const yMin = data.map(candle => candle.low);
 const yMax = data.map(candle => candle.high);
 const yVolume = data.map(candle => candle.volume);
 
-const regressionPrice = math.regression(model, x, yPrice, [1, 0.01, 0, avgPrice]);
-const regressionMin = math.regression(model, x, yMin, [1, 0.01, 0, avgMin]);
-const regressionMax = math.regression(model, x, yMax, [1, 0.01, 0, avgMax]);
-const regressionVolume = math.regression(model, x, yVolume, [1, 0.01, 0, avgVolume]);
+const optimizePrice = math.optimize(model, [1, 0.01, 0, avgPrice], x, yPrice);
+const optimizeMin = math.optimize(model, [1, 0.01, 0, avgMin], x, yMin);
+const optimizeMax = math.optimize(model, [1, 0.01, 0, avgMax], x, yMax);
+const optimizeVolume = math.optimize(model, [1, 0.01, 0, avgVolume], x, yVolume);
 
-const resultPrice = regressionPrice.equation;
-const resultMin = regressionMin.equation;
-const resultMax = regressionMax.equation;
-const resultVolume = regressionVolume.equation;
+const resultPrice = optimizePrice._data[0];
+const resultMin = optimizeMin._data[0];
+const resultMax = optimizeMax._data[0];
+const resultVolume = optimizeVolume._data[0];
 
 const aPrice = resultPrice.coefficients[0];
 const bPrice = resultPrice.coefficients[1];
