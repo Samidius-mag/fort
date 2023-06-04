67128d822e11ffbff7f4c6ec2f82fa074252ef23
Samidius <sdirt5895@gmail.com>
2023-06-04 11:44:09 +1000

    15614

--- 155.js
+++ 155.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
 
-const volume = data.map(candle => candle[5]);
-const numberOfTrades = data.map(candle => candle[8]);
+const volume = data.map(candle => parseFloat(candle.volume));
+const numberOfTrades = data.map(candle => parseFloat(candle.numberOfTrades));
 
 const averageVolume = volume.reduce((acc, val) => acc + val, 0) / volume.length;
 const averageNumberOfTrades = numberOfTrades.reduce((acc, val) => acc + val, 0) / numberOfTrades.length;
