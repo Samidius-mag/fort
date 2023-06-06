becd68e10febbdedc64166432acbecebec9bd79a
Samidius <sdirt5895@gmail.com>
2023-06-06 18:01:22 +1000

    212315

---
+++ botor.js
@@ -0,0 +1,31 @@
+/*
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
+const chatId = '-1001979484873';
+
+const bot = new TelegramBot(token, { polling: false });
+
+const sendMessage = (message) => {
+  bot.sendMessage(chatId, message);
+};
+*/
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// создаем экземпляр бота
+const bot = new TelegramBot('5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ', { polling: false });
+
+// ID пользователя, которому будет отправлено сообщение
+const chatId = '-1001979484873';
+// отправляем сообщение с текущим временем
+bot.sendMessage(chatId, `Текущее время: ${getCurrentTime()}`);
+
+// функция, которая возвращает текущее время
+function getCurrentTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  return `${hours}:${minutes}:${seconds}`;
+}
