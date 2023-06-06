c033d81bacbeee33759915c7704174d60c321170
Samidius <sdirt5895@gmail.com>
2023-06-06 16:08:40 +1000

    2123145

--- logic2.js
+++ logic2.js
@@ -146,11 +146,11 @@ const entryExitPoints = {
 if (buySell.RSI.buy && buySell.ROC.buy) {
   console.log('Точка входа в сделку (покупка):', entryExitPoints.buy.support);
   console.log('Точка выхода из сделки:', entryExitPoints.buy.resistance);
-  console.log('Стоп - лосс:', supportResistance['24h'].support)
+  console.log('Стоп - лосс:', supportResistance['4h'].support)
 } else if (buySell.RSI.sell && buySell.ROC.sell) {
   console.log('Точка входа в сделку (продажа):', entryExitPoints.sell.resistance);
   console.log('Точка выхода из сделки:', entryExitPoints.sell.support);
-  console.log('Стоп - лосс:', supportResistance['24h'].resistance)
+  console.log('Стоп - лосс:', supportResistance['4h'].resistance)
 }
 
 
