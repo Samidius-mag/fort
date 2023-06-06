ab5300d5a734d1268ed921bf0525e5f23ef8ece8
Samidius <sdirt5895@gmail.com>
2023-06-06 10:49:10 +1000

    32132123

--- firstlingreg.js
+++ firstlingreg.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { LinearRegression } = require('ml-regression');
+const { SimpleLinearRegression } = require('ml-regression');
 const rawData = fs.readFileSync('price.json');
 const data = JSON.parse(rawData);
 // Сортируем данные по убыванию количества сделок
@@ -13,9 +13,9 @@ const yValues = data.map((item) => parseInt(item.numberOfTrades));
 console.log('xValues:', xValues);
 console.log('yValues:', yValues);
 // Выполняем регрессию
-const regression = new LinearRegression(xValues.map((x) => [x]), yValues);
+const regression = new SimpleLinearRegression(xValues, yValues);
 console.log('Коэффициенты регрессии:', regression.slope, regression.intercept);
 // Предсказываем значения для следующих 500 свечей
 const nextXValues = Array.from({ length: 500 }, (_, i) => xValues[xValues.length - 1] + i + 1);
-const nextYValues = nextXValues.map((x) => regression.predict([x])[0]);
+const nextYValues = nextXValues.map((x) => regression.predict(x));
 console.log('Предсказанные значения:', nextYValues);
