407c374ac968496198d5bbea0efbd07b4d281228
Samidius <sdirt5895@gmail.com>
2023-06-06 19:35:14 +1000

    21235

--- botor.js
+++ botor.js
@@ -12,37 +12,35 @@ const sendMessage = (message) => {
 */
 
 const TelegramBot = require('node-telegram-bot-api');
-const { currentPrice, currentVolume, currentNumberOfTrades } = require('./logic3.js');
+const { currentPrice } = require('./logic3.js');
 // создаем экземпляр бота
 const bot = new TelegramBot('5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ', { polling: false });
 
 // ID пользователя, которому будет отправлено сообщение
 const chatId = '-1001979484873';
 // отправляем статическое сообщение с текущими значениями
-bot.sendMessage(chatId, `Текущая цена: ${currentPrice}\nТекущий объем: ${currentVolume}\nТекущее количество сделок: ${currentNumberOfTrades}`)
+bot.sendMessage(chatId, `Текущая цена: ${currentPrice}`)
   .then((message) => {
     // сохраняем ID сообщения для последующего обновления
     const messageId = message.message_id;
 
     // сохраняем предыдущие значения
     let prevPrice = currentPrice;
-    let prevVolume = currentVolume;
-    let prevNumberOfTrades = currentNumberOfTrades;
+  
 
     // обновляем сообщение каждые 10 секунд
     setInterval(() => {
       // проверяем, изменились ли значения
-      if (prevPrice !== currentPrice || prevVolume !== currentVolume || prevNumberOfTrades !== currentNumberOfTrades) {
+      if (prevPrice !== currentPrice) {
         // обновляем сообщение с новыми значениями
-        bot.editMessageText(`Текущая цена: ${currentPrice}\nТекущий объем: ${currentVolume}\nТекущее количество сделок: ${currentNumberOfTrades}`, {
+        bot.editMessageText(`Текущая цена: ${currentPrice}`, {
           chat_id: chatId,
           message_id: messageId,
         });
 
         // обновляем предыдущие значения
         prevPrice = currentPrice;
-        prevVolume = currentVolume;
-        prevNumberOfTrades = currentNumberOfTrades;
+        
       }
     }, 15000);
   });
--- logic3.js
+++ logic3.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const priceData = require('./price.json');
 const currentPrice = parseFloat(priceData[priceData.length - 1].close).toFixed(2);
-const volume = priceData.map(candle => parseFloat(candle.volume));
-const numberOfTrades = priceData.map(candle => parseFloat(candle.numberOfTrades));
-const currentVolume = volume[volume.length - 1];
-const currentNumberOfTrades = numberOfTrades[numberOfTrades.length - 1];
+//const volume = priceData.map(candle => parseFloat(candle.volume));
+//const numberOfTrades = priceData.map(candle => parseFloat(candle.numberOfTrades));
+//const currentVolume = volume[volume.length - 1];
+//const currentNumberOfTrades = numberOfTrades[numberOfTrades.length - 1];
 module.exports = {
   currentPrice,
-  currentVolume,
-  currentNumberOfTrades,
+  //currentVolume,
+  //currentNumberOfTrades,
 };
