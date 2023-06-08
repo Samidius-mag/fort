bdd8a7cbd0de69538fad85327a699469bf12f92d
Samidius <sdirt5895@gmail.com>
2023-06-09 08:18:02 +1000

    231215555

--- analize.js
+++ analize.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { RSI, SMA, Stochastic } = require('technicalindicators');
+const { RSI, EMA, Stochastic } = require('technicalindicators');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
 
@@ -7,10 +7,10 @@ const data = JSON.parse(fs.readFileSync('price.json'));
 function calculateIndicators(data) {
   const close = data.map(candle => candle.close);
   const rsi = RSI.calculate({ values: close, period: 14 });
-  const sma = SMA.calculate({ values: close, period: 20 });
+  const ema = EMA.calculate({ values: close, period: 20 });
   const stochastic = Stochastic.calculate({ high: data.map(candle => candle.high), low: data.map(candle => candle.low), close, period: 14, signalPeriod: 3 });
 
-  return { rsi, sma, stochastic };
+  return { rsi, ema, stochastic };
 }
 
 // Вычисляем индикаторы
@@ -19,20 +19,22 @@ const indicators = calculateIndicators(data);
 // Определяем текущую цену и последние значения индикаторов
 const lastCandle = data[data.length - 1];
 const lastRsi = indicators.rsi[indicators.rsi.length - 1];
-const lastSma = indicators.sma[indicators.sma.length - 1];
+const lastEma = indicators.ema[indicators.ema.length - 1];
 const lastStochastic = indicators.stochastic[indicators.stochastic.length - 1];
 
-// Выводим значения индикаторов в консоль
-console.log(`RSI: ${lastRsi.toFixed(2)}, SMA: ${lastSma.toFixed(2)}, Stochastic: %K: ${lastStochastic.k.toFixed(2)}, %D: ${lastStochastic.d.toFixed(2)}`);
+// Выводим текущие значения индикаторов
+console.log(`Current RSI: ${lastRsi}`);
+console.log(`Current EMA: ${lastEma}`);
+console.log(`Current Stochastic: %K=${lastStochastic.k}, %D=${lastStochastic.d}`);
 
 // Определяем условия для входа и выхода из рынка
 const isRsiOversold = lastRsi <= 30;
-const isPriceAboveSma = lastCandle.close > lastSma;
+const isPriceAboveEma = lastCandle.close > lastEma;
 const isStochasticBullish = lastStochastic.k > lastStochastic.d && lastStochastic.k < 20;
 const isStochasticBearish = lastStochastic.k < lastStochastic.d && lastStochastic.k > 80;
 
 // Если все условия для входа выполнены, сигнализируем о входе в рынок
-if (isRsiOversold && isPriceAboveSma && isStochasticBullish) {
+if (isRsiOversold && isPriceAboveEma && isStochasticBullish) {
   console.log('Buy signal detected!');
 } else if (isStochasticBearish) { // Если Stochastic Oscillator пересекает %D вниз, сигнализируем о выходе из рынка
   console.log('Sell signal detected!');
