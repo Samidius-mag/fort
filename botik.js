52be68aae3d2e80070957bf5e467ee7b84ad5905
Samidius <sdirt5895@gmail.com>
2023-06-08 16:48:18 +1000

    21135

---
+++ botik.js
@@ -0,0 +1,41 @@
+// Подключаем необходимые библиотеки
+const TelegramBot = require('node-telegram-bot-api');
+const { exec } = require('child_process');
+
+// Указываем токен телеграм-бота
+const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
+
+// Создаем экземпляр бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Указываем id чата, в который будем отправлять сообщения
+const chatId = '1001536433459';
+
+// Создаем функцию для отправки сообщений в чат
+let messageId = null; // хранит id последнего отправленного сообщения
+const sendMessage = (text) => {
+  if (messageId) {
+    bot.editMessageText(text, { chat_id: chatId, message_id: messageId });
+  } else {
+    bot.sendMessage(chatId, text).then((message) => {
+      messageId = message.message_id;
+    });
+  }
+};
+
+// Создаем функцию для выполнения файла x.js
+const runXScript = () => {
+  exec('node logic3.js', (error, stdout) => {
+    if (error) {
+      sendMessage(`Error: ${error.message}`);
+      return;
+    }
+
+    if (stdout) {
+      sendMessage(`Data from logic3.js: ${stdout}`);
+    }
+  });
+};
+
+// Запускаем выполнение файла x.js и отправку данных в чат
+runXScript();
