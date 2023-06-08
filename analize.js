71ea2bb4375dad44d12d1d96981b82897ef1ffe2
Samidius <sdirt5895@gmail.com>
2023-06-09 07:58:29 +1000

    15515

--- analize.js
+++ analize.js
@@ -1,28 +1,47 @@
 const fs = require('fs');
+const { MACD, RSI, BollingerBands, SMA } = require('technicalindicators');
 
-// Читаем файл indres.json
-fs.readFile('indres.json', (err, data) => {
-  if (err) throw err;
+const data = JSON.parse(fs.readFileSync('price.json'));
 
-  // Извлекаем массив значений объема OBV
-  const indRes = JSON.parse(data);
-  const obv = indRes.OBV;
+// Функция для вычисления индикаторов
+function calculateIndicators(data) {
+  const close = data.map(candle => candle.close);
+  const macd = MACD.calculate({ values: close, fastPeriod: 12, slowPeriod: 26, signalPeriod: 9 });
+  const rsi = RSI.calculate({ values: close, period: 14 });
+  const bb = BollingerBands.calculate({ values: close, period: 20, stdDev: 2 });
+  const sma = SMA.calculate({ values: close, period: 20 });
 
-  // Находим максимальное и минимальное значение объема
-  const maxVolume = Math.max(...obv);
-  const minVolume = Math.min(...obv);
+  return { macd, rsi, bb, sma };
+}
 
-  // Вычисляем среднее значение объема
-  const avgVolume = ((maxVolume + minVolume) / 2)/1000000;
+// Вычисляем индикаторы
+const indicators = calculateIndicators(data);
 
-  // Определяем, насколько текущий объем отличается от среднего значения
-  const currentVolume = (obv[obv.length - 1])/1000000;
-  const volumeDiff = currentVolume - avgVolume;
+// Определяем текущую цену и последние значения индикаторов
+const lastCandle = data[data.length - 1];
+const lastMacd = indicators.macd[indicators.macd.length - 1];
+const lastRsi = indicators.rsi[indicators.rsi.length - 1];
+const lastBb = indicators.bb[indicators.bb.length - 1];
+const lastSma = indicators.sma[indicators.sma.length - 1];
 
-  // Если текущий объем больше среднего значения, то это может свидетельствовать о продолжении тренда
-  if (volumeDiff > 0) {
-    console.log(`Текущий объем (${currentVolume.toFixed(5)}) больше среднего значения (${avgVolume.toFixed(5)}). Возможно, тренд продолжится.`);
-  } else {
-    console.log(`Текущий объем (${currentVolume.toFixed(5)}) меньше или равен среднему значению (${avgVolume.toFixed(5)}). Возможно, тренд завершится.`);
-  }
-});
+// Выводим значения индикаторов в консоль
+console.log(`MACD: ${lastMacd.MACD.toFixed(2)}, Signal: ${lastMacd.signal.toFixed(2)}, Histogram: ${lastMacd.histogram.toFixed(2)}`);
+console.log(`RSI: ${lastRsi.toFixed(2)}`);
+console.log(`Bollinger Bands: Upper: ${lastBb.upper.toFixed(2)}, Middle: ${lastBb.middle.toFixed(2)}, Lower: ${lastBb.lower.toFixed(2)}`);
+console.log(`SMA: ${lastSma.toFixed(2)}`);
+
+// Определяем условия для входа и выхода из рынка
+const isMacdBullish = lastMacd.MACD > lastMacd.signal;
+const isRsiOversold = lastRsi <= 30;
+const isPriceAboveBbLower = lastCandle.close > lastBb.lower;
+const isPriceAboveSma = lastCandle.close > lastSma;
+const isMacdBearish = lastMacd.MACD < lastMacd.signal;
+
+// Если все условия для входа выполнены, сигнализируем о входе в рынок
+if (isMacdBullish && isRsiOversold && isPriceAboveBbLower && isPriceAboveSma) {
+  console.log('Buy signal detected!');
+} else if (isMacdBearish) { // Если MACD пересекает сигнальную линию вниз, сигнализируем о выходе из рынка
+  console.log('Sell signal detected!');
+} else {
+  console.log('No buy or sell signal detected');
+}
