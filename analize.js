6beebeace3f7c322ce8977bb0b2b5087c9b25e4f
Samidius <sdirt5895@gmail.com>
2023-06-09 11:32:36 +1000

    21231555555

--- analize.js
+++ analize.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const { RSI, EMA, Stochastic, MACD } = require('technicalindicators');
-
 const data = JSON.parse(fs.readFileSync('price.json'));
-
 // Функция для вычисления индикаторов
 function calculateIndicators(data) {
   const close = data.map(candle => parseFloat(candle.close));
@@ -14,26 +12,16 @@ function calculateIndicators(data) {
   const emaValues = EMA.calculate({ period: emaPeriod, values: close });
   const ema = emaValues.map(value => parseFloat(value.toFixed(2)));
   const stochastic = Stochastic.calculate({ high: data.map(candle => candle.high), low: data.map(candle => candle.low), close, period: 14, signalPeriod: 3 });
-  //console.log(macd);
   return { rsi, ema, stochastic, macd };
 }
-
 // Вычисляем индикаторы
 const indicators = calculateIndicators(data);
-
 // Определяем текущую цену и последние значения индикаторов
 const lastCandle = data[data.length - 1];
 const lastRsi = indicators.rsi[indicators.rsi.length - 1];
 const lastEma = indicators.ema[indicators.ema.length - 1];
 const lastStochastic = indicators.stochastic[indicators.stochastic.length - 1];
 const lastMacd = indicators.macd[indicators.macd.length - 1];
-
-// Выводим текущие значения индикаторов
-console.log(`Current RSI: ${lastRsi}`);
-console.log(`Current EMA: ${lastEma}`);
-console.log(`Current Stochastic: %K=${lastStochastic.k}, %D=${lastStochastic.d}`);
-console.log(`Current MACD: ${lastMacd.MACD}`);
-
 // Определяем условия для входа и выхода из рынка
 const isRsiOversold = lastRsi <= 30;
 const isPriceAboveEma = lastCandle.close > lastEma;
@@ -41,7 +29,6 @@ const isStochasticBullish = lastStochastic.k > lastStochastic.d && lastStochasti
 const isStochasticBearish = lastStochastic.k < lastStochastic.d && lastStochastic.k > 80;
 const isMacdBullish = lastMacd.MACD > lastMacd.signal && lastMacd.histogram > 0;
 const isMacdBearish = lastMacd.MACD < lastMacd.signal && lastMacd.histogram < 0;
-
 // Если все условия для входа выполнены, сигнализируем о входе в рынок
 if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish) {
   console.log('Найден сигнал на покупку!');
--- logic2.js
+++ logic2.js
@@ -265,8 +265,9 @@ const isStochasticBullish = lastStochastic.k > lastStochastic.d && lastStochasti
 const isStochasticBearish = lastStochastic.k < lastStochastic.d && lastStochastic.k > 80;
 const isMacdBullish = lastMacd.MACD > lastMacd.signal && lastMacd.histogram > 0;
 const isMacdBearish = lastMacd.MACD < lastMacd.signal && lastMacd.histogram < 0;
+const isVolumeBullish = averageVolume < currentVolume
 // Если все условия для входа выполнены, сигнализируем о входе в рынок
-if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish && averageVolume < currentVolume) {
+if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish && isVolumeBullish) {
   console.log('Найден сигнал на покупку!');
 } else if (isStochasticBearish || isMacdBearish) { // Если Stochastic Oscillator или MACD пересекает сигнальную линию вниз, сигнализируем о выходе из рынка
   console.log('Найден сигнал на продажу!');
