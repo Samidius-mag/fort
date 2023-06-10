727426c1f91efd1e5fe0ec0df32266a2568ad928
Samidius <sdirt5895@gmail.com>
2023-06-10 18:15:57 +1000

    123555

--- logic5.js
+++ logic5.js
@@ -11,70 +11,35 @@ const bot = new TelegramBot(token, { polling: false });
 
 let isSignalSent = false; // Переменная для хранения информации о том, было ли уже отправлено сообщение
 
-// Функция для проверки условий на вход в рынок
-
-    // Функция для вычисления индикаторов
-function calculateIndicators(data) {
-    const close = data.map(candle => parseFloat(candle.close));
-    const macd = MACD.calculate({ values: close, fastPeriod: 12, slowPeriod: 26, signalPeriod: 9 });
-    const rsiPeriod = 14;
-    const rsiValues = RSI.calculate({ period: rsiPeriod, values: close });
-    const rsi = rsiValues.map(value => parseFloat(value.toFixed(2)));
-    const emaPeriod = 21;
-    const emaValues = EMA.calculate({ period: emaPeriod, values: close });
-    const ema = emaValues.map(value => parseFloat(value.toFixed(2)));
-    const stochastic = Stochastic.calculate({ high: data.map(candle => candle.high), low: data.map(candle => candle.low), close, period: 14, signalPeriod: 3 });
-    return { rsi, ema, stochastic, macd };
+const indicators = calculateIndicators(data);
+const lastCandle = data[data.length - 1];
+const lastRsi = indicators.rsi[indicators.rsi.length - 1];
+const lastEma = indicators.ema[indicators.ema.length - 1];
+const lastStochastic = indicators.stochastic[indicators.stochastic.length - 1];
+const lastMacd = indicators.macd[indicators.macd.length - 1];
+const isRsiOversold = lastRsi <= 30;
+const isPriceAboveEma = lastCandle.close > lastEma;
+const isStochasticBullish = lastStochastic.k > lastStochastic.d && lastStochastic.k < 20;
+const isStochasticBearish = lastStochastic.k < lastStochastic.d && lastStochastic.k > 80;
+const isMacdBullish = lastMacd.MACD > lastMacd.signal && lastMacd.histogram > 0;
+const isMacdBearish = lastMacd.MACD < lastMacd.signal && lastMacd.histogram < 0;
+const isVolumeBullish = averageVolume < currentVolume
+// Если все условия для входа выполнены, сигнализируем о входе в рынок
+if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish && isVolumeBullish) {
+console.log('Найден сигнал на покупку!');
+} else if (isStochasticBearish || isMacdBearish && isVolumeBullish) { // Если Stochastic Oscillator или MACD пересекает сигнальную линию вниз, сигнализируем о выходе из рынка
+console.log('Найден сигнал на продажу!');
 }
-  // Вычисляем индикаторы
-  const indicators = calculateIndicators(data);
-  // Определяем текущую цену и последние значения индикаторов
-  const lastCandle = data[data.length - 1];
-  const lastRsi = indicators.rsi[indicators.rsi.length - 1];
-  const lastEma = indicators.ema[indicators.ema.length - 1];
-  const lastStochastic = indicators.stochastic[indicators.stochastic.length - 1];
-  const lastMacd = indicators.macd[indicators.macd.length - 1];
-  function checkRsiOversold() {
-    const isRsiOversold = lastRsi <= 30;
-  }
-  function checkPriceAboveEma() {
-    const isPriceAboveEma = lastCandle.close > lastEma;
-  }
-  function checkStochasticBullish() {
-    const isStochasticBullish = lastStochastic.k > lastStochastic.d && lastStochastic.k < 20;
-  const isStochasticBearish = lastStochastic.k < lastStochastic.d && lastStochastic.k > 80;
-  }
-  function checkMacdBullish() {
-    const isMacdBullish = lastMacd.MACD > lastMacd.signal && lastMacd.histogram > 0;
-  const isMacdBearish = lastMacd.MACD < lastMacd.signal && lastMacd.histogram < 0;
-  }
-  function checkVolumeBullish() {
-    const isVolumeBullish = averageVolume < currentVolume
-  }
-  
-/* Запускаем бота
-bot.start((ctx) => ctx.reply('Привет! Я бот для отправки сигналов на вход в рынок.'));
-bot.launch();
-*/
-// Отправляем сообщение, если выполняются условия для входа в рынок
-setInterval(() => {
-  const isRsiOversold = checkRsiOversold();
-  const isPriceAboveEma = checkPriceAboveEma();
-  const isStochasticBullish = checkStochasticBullish();
-  const isMacdBullish = checkMacdBullish();
-  const isVolumeBullish = checkVolumeBullish();
 
-  if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish && isVolumeBullish) {
-    if (!isSignalSent) { // Проверяем, было ли уже отправлено сообщение
-      bot.telegram.sendMessage(chatId, 'Найден сигнал на покупку!');
-      isSignalSent = true; // Устанавливаем флаг, что сообщение было отправлено
-    }
-  } else if (isStochasticBearish || isMacdBearish && isVolumeBullish) {
-    if (!isSignalSent) { // Проверяем, было ли уже отправлено сообщение
-      bot.telegram.sendMessage(chatId, 'Найден сигнал на продажу!');
-      isSignalSent = true; // Устанавливаем флаг, что сообщение было отправлено
-    }
-  } else {
-    isSignalSent = false; // Сбрасываем флаг, если условия для отправки сообщения больше не выполняются
+// Если все условия для входа выполнены, сигнализируем о входе в рынок
+if (isRsiOversold && isPriceAboveEma && isStochasticBullish && isMacdBullish && isVolumeBullish) {
+  if (!isSignalSent) { // Проверяем, было ли уже отправлено сообщение
+    bot.sendMessage(chatId, 'Найден сигнал на покупку!');
+    isSignalSent = true; // Устанавливаем значение переменной isSignalSent в true, чтобы сообщение не отправлялось заново
   }
-}, 60000); // Проверяем условия каждую минуту
+} else if (isStochasticBearish || isMacdBearish && isVolumeBullish) { // Если Stochastic Oscillator или MACD пересекает сигнальную линию вниз, сигнализируем о выходе из рынка
+  if (!isSignalSent) { // Проверяем, было ли уже отправлено сообщение
+    bot.sendMessage(chatId, 'Найден сигнал на продажу!');
+    isSignalSent = true; // Устанавливаем значение переменной isSignalSent в true, чтобы сообщение не отправлялось заново
+  }
+}
