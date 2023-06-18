1268ac28ad6c7c081b2fd132115fef40082ca3ba
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-06-18 18:59:14 +0600

    4688

--- nelinreg.js
+++ nelinreg.js
@@ -12,7 +12,7 @@ const highVolumeCandles = data.filter(candle => candle.volume > averageVolume);
 const lowVolumeCandles = data.filter(candle => candle.volume <= averageVolume);
 
 // Считаем коэффициент корреляции между объемом и изменением цены для каждой группы
-const highVolumeCorrelation = correlation(highVolumeCandles.map(candle => candle.volume), highVolumeCandles.map(candle => candle.close - candle.open));
+const highVolumeCorrelation = ss.sampleCorrelation(highVolumeCandles.map(candle => candle.volume), highVolumeCandles.map(candle => candle.close - candle.open));
 const lowVolumeCorrelation = correlation(lowVolumeCandles.map(candle => candle.volume), lowVolumeCandles.map(candle => candle.close - candle.open));
 
 // Определяем закономерность между объемом, корреляцией и направлением движения рынка
