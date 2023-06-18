934d9af9749ab6698d57fe26b1b4174d8d3738d5
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-06-18 18:45:37 +0600

    fklja

--- nelinreg.js
+++ nelinreg.js
@@ -11,28 +11,58 @@ const averageVolume = volume.reduce((acc, val) => acc + val, 0) / volume.length;
 const highVolumeCandles = data.filter(candle => candle.volume > averageVolume);
 const lowVolumeCandles = data.filter(candle => candle.volume <= averageVolume);
 
-// Считаем количество свечей с ростом цены и спадом цены в каждой группе
-const highVolumeCandlesUp = highVolumeCandles.filter(candle => candle.close > candle.open).length;
-const highVolumeCandlesDown = highVolumeCandles.filter(candle => candle.close < candle.open).length;
-const lowVolumeCandlesUp = lowVolumeCandles.filter(candle => candle.close > candle.open).length;
-const lowVolumeCandlesDown = lowVolumeCandles.filter(candle => candle.close < candle.open).length;
+// Считаем коэффициент корреляции между объемом и изменением цены для каждой группы
+const highVolumeCorrelation = correlation(highVolumeCandles.map(candle => candle.volume), highVolumeCandles.map(candle => candle.close - candle.open));
+const lowVolumeCorrelation = correlation(lowVolumeCandles.map(candle => candle.volume), lowVolumeCandles.map(candle => candle.close - candle.open));
 
-// Определяем закономерность между объемом и направлением движения рынка
+// Определяем закономерность между объемом, корреляцией и направлением движения рынка
 let trend;
-if (highVolumeCandlesUp > highVolumeCandlesDown) {
-  trend = 'при увеличении объема рынок склонен к росту цены';
-} else if (highVolumeCandlesUp < highVolumeCandlesDown) {
-  trend = 'при увеличении объема рынок склонен к спаду цены';
+let correlationTrend;
+if (highVolumeCorrelation > lowVolumeCorrelation) {
+  correlationTrend = 'при увеличении объема корреляция между объемом и изменением цены увеличивается';
+  trend = data.slice(-2)[0].close > data.slice(-2)[0].open ? 'up' : 'down';
+  if (highVolumeCandles.filter(candle => candle.close > candle.open).length > highVolumeCandles.filter(candle => candle.close < candle.open).length) {
+    correlationTrend += ' и рынок склонен к росту цены';
+    if (trend === 'up') {
+      trend = 'подтверждается текущий тренд';
+    } else {
+      trend = 'текущий тренд может измениться на рост цены';
+    }
+  } else {
+    correlationTrend += ' и рынок склонен к спаду цены';
+    if (trend === 'down') {
+      trend = 'подтверждается текущий тренд';
+    } else {
+      trend = 'текущий тренд может измениться на спад цены';
+    }
+  }
+} else if (highVolumeCorrelation < lowVolumeCorrelation) {
+  correlationTrend = 'при увеличении объема корреляция между объемом и изменением цены уменьшается';
+  trend = data.slice(-2)[0].close > data.slice(-2)[0].open ? 'down' : 'up';
+  if (highVolumeCandles.filter(candle => candle.close > candle.open).length > highVolumeCandles.filter(candle => candle.close < candle.open).length) {
+    correlationTrend += ' и рынок склонен к спаду цены';
+    if (trend === 'down') {
+      trend = 'подтверждается текущий тренд';
+    } else {
+      trend = 'текущий тренд может измениться на спад цены';
+    }
+  } else {
+    correlationTrend += ' и рынок склонен к росту цены';
+    if (trend === 'up') {
+      trend = 'подтверждается текущий тренд';
+    } else {
+      trend = 'текущий тренд может измениться на рост цены';
+    }
+  }
 } else {
+  correlationTrend = 'нет закономерности между объемом и изменением цены';
   trend = 'нет закономерности между объемом и направлением движения рынка';
 }
 
-console.log(`Средний объем за последние 100 свечей: ${averageVolume}`);
+console.log(`Средний объем: ${averageVolume}`);
 console.log(`Текущий тренд: ${data.slice(-2)[0].close > data.slice(-2)[0].open ? 'up' : 'down'}`);
 console.log(`Количество свечей с объемом выше среднего: ${highVolumeCandles.length}`);
 console.log(`Количество свечей с объемом ниже или равным среднему: ${lowVolumeCandles.length}`);
-console.log(`Количество свечей с ростом цены при объеме выше среднего: ${highVolumeCandlesUp}`);
-console.log(`Количество свечей со спадом цены при объеме выше среднего: ${highVolumeCandlesDown}`);
-console.log(`Количество свечей с ростом цены при объеме ниже или равном среднему: ${lowVolumeCandlesUp}`);
-console.log(`Количество свечей со спадом цены при объеме ниже или равном среднему: ${lowVolumeCandlesDown}`);
-console.log(`Закономерность между объемом и направлением движения рынка: ${trend}`);
+console.log(`Коэффициент корреляции между объемом и изменением цены при объеме выше среднего: ${highVolumeCorrelation}`);
+console.log(`Коэффициент корреляции между объемом и изменением цены при объеме ниже или равном среднему: ${lowVolumeCorrelation}`);
+console.log(`Закономерность между объемом, корреляцией и направлением движения рынка: ${correlationTrend}, ${trend}`);
