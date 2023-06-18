e82945e7d55603eded9afeba3354e4956a26c808
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-06-18 11:30:01 +0300

    Update nelinreg.js

--- nelinreg.js
+++ nelinreg.js
@@ -1,30 +1,16 @@
-const math = require('mathjs');
 const fs = require('fs');
-const data = require('./price.json');
-const x = data.map(candle => candle.close);
-const y = data.map(candle => candle.low);
-const n = x.length;
-const sumX = math.sum(x);
-const sumY = math.sum(y);
-const sumXY = math.sum(math.dotMultiply(x, y));
-const sumX2 = math.sum(math.dotPow(x, 2));
-const b = (n * sumXY - sumX * sumY) / (n * sumX2 - math.pow(sumX, 2));
-const a = (sumY - b * sumX) / n;
-console.log('a:', a); // отладочный вывод
-console.log('b:', b); // отладочный вывод
-const nextPrice = a + b * x[x.length - 1];
-console.log('Цена регрессии (линейная):', nextPrice); // отладочный вывод
-const x1 = data.map(candle => candle.close);
-const y1 = data.map(candle => candle.low);
-const n1 = x1.length;
-const sumX1 = math.sum(x1);
-const sumY1 = math.sum(y1);
-const sumXY1 = math.sum(math.dotMultiply(x1, y1));
-const sumX21 = math.sum(math.dotPow(x1, 2));
-const b1 = (n1 * sumXY1 - sumX1 * sumY1) / (n1 * sumX21 - math.pow(sumX1, 2));
-const a1 = math.mean(y1) / math.pow(math.mean(x), b1);
-const nextPrice1 = a1 * Math.pow(x[x.length - 1], b1);
-console.log(`Цена регрессии (нелинейная): ${nextPrice1}`);
 
-// сохраняем данные в файл regres.json с добавлением
-fs.appendFileSync('regres.json', JSON.stringify({linear: nextPrice, nonlinear: nextPrice1}) + '\n');
+const data = JSON.parse(fs.readFileSync('price.json'));
+
+// Считаем средний объем за последние 100 свечей
+const averageVolume = data.slice(-100).reduce((sum, candle) => sum + candle.volume, 0) / 100;
+
+// Определяем текущий тренд
+const currentTrend = data.slice(-2)[0].close > data.slice(-2)[0].open ? 'up' : 'down';
+
+// Считаем количество свечей, в которых объем был выше среднего
+const highVolumeCandles = data.filter(candle => candle.volume > averageVolume).length;
+
+console.log(`Средний объем за последние 100 свечей: ${averageVolume}`);
+console.log(`Текущий тренд: ${currentTrend}`);
+console.log(`Количество свечей с объемом выше среднего: ${highVolumeCandles}`);
