86e2d60b776b906ca1c0eaccadbf935ee325c9ac
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-06-21 11:18:36 +0600

    2123155

--- volema.js
+++ volema.js
@@ -1,67 +1,47 @@
-const fs = require('fs');
+const data = require('./price.json');
 
-const rawData = fs.readFileSync('price.json');
-const data = JSON.parse(rawData);
+const capital = 100;
+const amountPerTrade = 0.01;
+const commission = 0.001;
 
-const candles = data.map(candle => ({
-  time: new Date(candle.time),
-  open: parseFloat(candle.open),
-  high: parseFloat(candle.high),
-  low: parseFloat(candle.low),
-  close: parseFloat(candle.close),
-  volume: parseFloat(candle.volume),
-}));
+let balance = capital;
+let holdings = 0;
+let lastClose = 0;
 
-function calculateSMA(data, n) {
-  const sum = data.slice(-n).reduce((sum, candle) => sum + candle.close, 0);
-  return sum / n;
-}
+for (let i = 0; i < data.length; i++) {
+  const candle = data[i];
+  const close = parseFloat(candle.close);
 
-function calculateSD(data, n) {
-  const sma = calculateSMA(data, n);
-  const sum = data.slice(-n).reduce((sum, candle) => sum + Math.pow(candle.close - sma, 2), 0);
-  return Math.sqrt(sum / n);
-}
-
-function calculateBollingerBands(data, n, k) {
-  const sma = calculateSMA(data, n);
-  const sd = calculateSD(data, n);
-  const upperBand = sma + k * sd;
-  const lowerBand = sma - k * sd;
-  return { upperBand, lowerBand };
-}
-
-let inTrade = false;
-let entryPrice = 0;
-let exitPrice = 0;
-let capital = 100;
-let trades = 0;
-let successfulTrades = 0;
-
-for (let i = 20; i < candles.length; i++) {
-  const { upperBand, lowerBand } = calculateBollingerBands(candles.slice(0, i), 20, 2);
-
-  if (!inTrade && candles[i].close <= lowerBand) {
-    inTrade = true;
-    entryPrice = candles[i].close;
+  if (lastClose === 0) {
+    lastClose = close;
+    continue;
   }
 
-  if (inTrade && candles[i].close >= upperBand) {
-    inTrade = false;
-    exitPrice = candles[i].close;
+  if (close > lastClose) {
+    const amount = amountPerTrade;
+    const cost = amount * close;
+    const fee = cost * commission;
+    const totalCost = cost + fee;
 
-    const profit = (exitPrice - entryPrice) / entryPrice * capital;
-    capital += profit;
-
-    if (profit > 0) {
-      successfulTrades++;
+    if (totalCost <= balance) {
+      balance -= totalCost;
+      holdings += amount;
+      console.log(`Bought ${amount} BTC at ${close}`);
     }
+  } else if (close < lastClose) {
+    const amount = holdings * amountPerTrade;
+    const revenue = amount * close;
+    const fee = revenue * commission;
+    const totalRevenue = revenue - fee;
 
-    trades++;
+    if (totalRevenue > 0) {
+      balance += totalRevenue;
+      holdings -= amount;
+      console.log(`Sold ${amount} BTC at ${close}`);
+    }
   }
+
+  lastClose = close;
 }
 
-console.log(`Total trades: ${trades}`);
-console.log(`Successful trades: ${successfulTrades}`);
-console.log(`Unsuccessful trades: ${trades - successfulTrades}`);
-console.log(`Final capital: ${capital.toFixed(2)}`);
+console.log(`Final balance: ${balance.toFixed(2)}`);
